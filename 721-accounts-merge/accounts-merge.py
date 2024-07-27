17b3cb3d9404793675f1e99f6fa814095bbe3680
andrewaucie <110870107+andrewaucie@users.noreply.github.com>
2024-07-27 11:23:26 -0400

    Time: 162 ms (94.43%) | Memory: 20.3 MB (63.68%) - LeetSync

--- 721-accounts-merge/accounts-merge.py
+++ 721-accounts-merge/accounts-merge.py
@@ -1,33 +1,29 @@
+class UF:
+    def __init__(self, N):
+        self.parents = list(range(N))
+    def union(self, child, parent):
+        self.parents[self.find(child)] = self.find(parent)
+    def find(self, x):
+        if x != self.parents[x]:
+            self.parents[x] = self.find(self.parents[x])
+        return self.parents[x]
+    
 class Solution:
+    # 196 ms, 82.09%. 
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
-        adjList = defaultdict(set)
-        for account in accounts:
-            for i,j in combinations(range(1, len(account)), 2):
-                adjList[account[i]].add(account[j])
-                adjList[account[j]].add(account[i])
-
-        visited = defaultdict(bool)
-        def bfs(visited, account):
-            queue = deque()
-            for i in range(1, len(account)):
-                if visited[account[i]] is False:
-                    queue.append(account[i])
-                    visited[account[i]] = True
-            account = [account[0]]
-            while queue:
-                curr = queue.popleft()
-                account.append(curr)
-                for e in adjList[curr]:
-                    if visited[e] is False:
-                        visited[e] = True
-                        queue.append(e)
-            return account
+        uf = UF(len(accounts))
         
-        newAccounts = []
-        for account in accounts:
-            if visited[account[1]] == False:
-                mergedAccount = bfs(visited, account)
-                mergedAccount = [account[0]] + sorted(mergedAccount[1:])
-                newAccounts.append(mergedAccount)
-        return newAccounts
-
+        # Creat unions between indexes
+        ownership = {}
+        for i, (_, *emails) in enumerate(accounts):
+            for email in emails:
+                if email in ownership:
+                    uf.union(i, ownership[email])
+                ownership[email] = i
+        
+        # Append emails to correct index
+        ans = collections.defaultdict(list)
+        for email, owner in ownership.items():
+            ans[uf.find(owner)].append(email)
+        
+        return [[accounts[i][0]] + sorted(emails) for i, emails in ans.items()]
