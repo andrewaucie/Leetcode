3baf3fd2e7c26c30e4f345d5297297b08a72e0b6
andrewaucie <110870107+andrewaucie@users.noreply.github.com>
2024-07-28 21:49:52 -0400

    Time: 47 ms (8.62%) | Memory: 16.6 MB (61.57%) - LeetSync

--- 269-alien-dictionary/alien-dictionary.py
+++ 269-alien-dictionary/alien-dictionary.py
@@ -1,30 +1,32 @@
 class Solution:
     def alienOrder(self, words: List[str]) -> str:
-        adj = { c: set() for w in words for c in w }
-        for i in range(len(words) - 1):
-            w1, w2 = words[i], words[i+1]
-            minLen = min(len(w1), len(w2))
-            if len(w1) > len(w2) and w1[:minLen] == w2[:minLen]:
-                return ""
-            for j in range(minLen):
-                if w1[j] != w2[j]:
-                    adj[w1[j]].add(w2[j])
-                    break
-        visit = {}
-        res = []
-
-        def dfs(c):
-            if c in visit:
-                return visit[c]
-            visit[c] = True
-            for nei in adj[c]:
-                if dfs(nei):
-                    return True
-            visit[c] = False
-            res.append(c)
+        n = len(words)
+        g = [[] for _ in range(26)]
+        in_degree = {}
+        for w in words:
+            for c in w: in_degree[ord(c) - ord("a")] = 0
         
-        for c in adj:
-            if dfs(c):
-                return ""
-        res.reverse()
-        return "".join(res)
+        for i in range(1, n):
+            w1, w2 = words[i - 1], words[i]
+            j = 0
+            while j < len(w1) and j < len(w2):
+                w1_idx, w2_idx = ord(w1[j]) - ord("a"), ord(w2[j]) - ord("a")
+                if w1_idx != w2_idx:
+                    g[w1_idx].append(w2_idx)
+                    in_degree[w2_idx] += 1
+                    break
+                j += 1
+            if j == len(w2) and j < len(w1): return ""
+        
+        pq = collections.deque()
+        for i in in_degree:
+            if in_degree[i] == 0: pq.append(i)
+        
+        res = []
+        while pq:
+            cur = pq.popleft()
+            res.append(chr(cur + ord("a")))
+            for nxt in g[cur]:
+                in_degree[nxt] -= 1
+                if in_degree[nxt] == 0: pq.append(nxt)
+        return "".join(res) if sum(in_degree.values()) == 0 else ""
