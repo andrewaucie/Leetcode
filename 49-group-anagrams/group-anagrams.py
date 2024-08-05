5ef95b7ff261924a456d6d25f962383c80f61889
andrewaucie <110870107+andrewaucie@users.noreply.github.com>
2024-08-04 21:58:22 -0400

    Time: 80 ms (92.64%) | Memory: 20.6 MB (39.11%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,14 +1,6 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        res = []
-        freqSet = {}
-        i = 0
+        res = defaultdict(list)
         for s in strs:
-            count = tuple(sorted(Counter(s).items()))
-            if count not in freqSet:
-                freqSet[count] = i
-                res.append([s])
-                i += 1
-            else:
-                res[freqSet[count]].append(s)
-        return res
+            res[tuple(sorted(s))].append(s)
+        return res.values()
