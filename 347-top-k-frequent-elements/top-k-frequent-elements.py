12e891954584be2e97bf992432be06468e414db8
andrewaucie <110870107+andrewaucie@users.noreply.github.com>
2024-08-06 20:08:17 -0400

    Time: 96 ms (23.29%) | Memory: 21.4 MB (34.96%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,7 +1,26 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        freqCount = Counter(nums)
-        freqList = sorted(freqCount.items(), key=lambda x:x[1], reverse=True)
-        freqList = [x[0] for x in freqList]
-        print(freqList)
-        return freqList[:k]
+        freq = defaultdict(int)
+        freqMax = 0
+        for num in nums:
+            freq[num] += 1
+            freqMax = max(freqMax, freq[num])
+        buckets = [[float("-inf")]] * (freqMax+1)
+        for f in freq:
+            if buckets[freq[f]] == [float("-inf")]:
+                buckets[freq[f]] = [f]
+            else:
+                buckets[freq[f]].append(f)
+        res = []
+        print(buckets)
+        for i in range(freqMax, -1, -1):
+            if k == 0:
+                break
+            if buckets[i] != [float("-inf")]:
+                if k - len(buckets[i]) >= 0:
+                    res.extend(buckets[i])
+                    k -= len(buckets[i])
+                else:
+                    res.extend(buckets[i][:k])
+                    k = 0
+        return res
