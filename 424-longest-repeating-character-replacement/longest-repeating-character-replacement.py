64599f233974d8380e3e3cfe8b57fb8f995ca390
andrewaucie <110870107+andrewaucie@users.noreply.github.com>
2024-08-08 19:01:05 -0400

    Time: 843 ms (5.00%) | Memory: 16.6 MB (32.23%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,30 +1,20 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        all_letters = set(s)
-        max_length = 0
-
-        # iterate over each unique letter
-        for letter in all_letters:
-            start = 0
-            count = 0
-
-            # initialize a sliding window for each unique letter
-            for end in range(len(s)):
-                if s[end] == letter:
-                    # if the letter matches, increase the count
-                    count += 1
-
-                # bring start forward until the window is valid again
-                while not self.__is_window_valid(start, end, count, k):
-                    if s[start] == letter:
-                        # if the letter matches, decrease the count
-                        count -= 1
-                    start += 1
-
-                # at this point the window is valid, update max_length
-                max_length = max(max_length, end + 1 - start)
-
-        return max_length
-
-    def __is_window_valid(self, start: int, end: int, count: int, k: int):
-        return end + 1 - start - count <= k
+        ans = 0
+        n = len(s)
+        for c in range(ord('A'), ord('Z') + 1):
+            c = chr(c)
+            i, j, replaced = 0, 0, 0
+            while j < n:
+                if s[j] == c:
+                    j += 1
+                elif replaced < k:
+                    j += 1
+                    replaced += 1
+                elif s[i] == c:
+                    i += 1
+                else:
+                    i += 1
+                    replaced -= 1
+                ans = max(ans, j - i)
+        return ans
