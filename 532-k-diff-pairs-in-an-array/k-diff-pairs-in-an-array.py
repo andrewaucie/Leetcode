2569cd9f09ced8591a92bacdd4a5ad4f2ac86f77
andrewaucie <110870107+andrewaucie@users.noreply.github.com>
2024-08-20 22:21:56 -0400

    Time: 86 ms (16.71%) | Memory: 19.1 MB (20.22%) - LeetSync

---
+++ 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findPairs(self, nums: List[int], k: int) -> int:
+        # | nums[i] - nums[j] | = k => nums[i] = nums[j] + k or nums[j] - k
+        # create numsSet
+        # at index i, check if nums[j] + k exists in numsSet
+        # if exists, mark (nums[i], nums[j]) as set
+        indexMap = {nums[i]: i for i in range(len(nums))}
+        pairs = 0
+        visitedPairs = set()
+        for i in range(len(nums)):
+            if nums[i] + k in indexMap and indexMap[nums[i]+k] != i:
+                sortedTuple = tuple(sorted([nums[i], nums[i]+k]))
+                if sortedTuple not in visitedPairs:
+                    visitedPairs.add(sortedTuple)
+            if nums[i] - k in indexMap and indexMap[nums[i]-k] != i:
+                sortedTuple = tuple(sorted([nums[i], nums[i]-k]))
+                if sortedTuple not in visitedPairs:
+                    visitedPairs.add(sortedTuple)
+        return len(visitedPairs)
