9955e0c773ce4185682e73e174862905d9c1ba6a
andrewaucie <110870107+andrewaucie@users.noreply.github.com>
2024-08-20 22:27:48 -0400

    Time: 59 ms (95.09%) | Memory: 18 MB (54.88%) - LeetSync

--- 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.py
+++ 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.py
@@ -1,15 +1,16 @@
 class Solution:
     def findPairs(self, nums: List[int], k: int) -> int:
-        # | nums[i] - nums[j] | = k => nums[i] = nums[j] + k or nums[j] - k
-        # create numsSet
-        # at index i, check if nums[j] + k exists in numsSet
-        # if exists, mark (nums[i], nums[j]) as set
-        indexMap = {nums[i]: i for i in range(len(nums))}
-        pairs = 0
-        visitedPairs = set()
-        for i in range(len(nums)):
-            if nums[i] + k in indexMap and indexMap[nums[i]+k] != i:
-                sortedTuple = tuple(sorted([nums[i], nums[i]+k]))
-                if sortedTuple not in visitedPairs:
-                    visitedPairs.add(sortedTuple)
-        return len(visitedPairs)
+        # return the number of unique pairs
+        answ=0
+        # create a dictionary: d[x]==nums.count(x)
+        d={}
+        for x in nums:
+            if x in d: d[x]+=1
+            else:      d[x]=1
+        
+        if k: # k>0
+            answ=sum(x+k in d for x in d.keys())
+        else: # k==0
+            answ=sum(k>1 for k in d.values())
+                
+        return answ
