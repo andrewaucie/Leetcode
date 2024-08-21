abed09bacbbc650a10a082ab746f5172af07a5c6
andrewaucie <110870107+andrewaucie@users.noreply.github.com>
2024-08-20 23:14:53 -0400

    Time: 139 ms (88.60%) | Memory: 19.7 MB (32.51%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.py
+++ 498-diagonal-traverse/diagonal-traverse.py
@@ -1,35 +1,13 @@
 class Solution:
   def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
-    # Step 1
-    result = []
-    # Step 2
-    m, n = len(mat), len(mat[0])
-    i = j = direction = 0
-    
-    # Step 3
-    for _ in range(m * n):
-        result.append(mat[i][j])
-        
-        if direction == 0: # Up-right direction
-            if j == n - 1:
-                direction = 1 # Change direction to down-left
-                i += 1
-            elif i == 0:
-                direction = 1 # Change direction to down-left
-                j += 1
-            else:
-                i -= 1
-                j += 1
-        else: # Down-left direction
-            if i == m - 1:
-                direction = 0 # Change direction to up-right
-                j += 1
-            elif j == 0:
-                direction = 0 # Change direction to up-right
-                i += 1
-            else:
-                i += 1
-                j -= 1
-    
-    # Step 4
-    return result
+    res = []
+    indexMap = defaultdict(list)
+    for i in range(len(mat)):
+        for j in range(len(mat[0])):
+            indexMap[i+j].append(mat[i][j])
+    for index, val in indexMap.items():
+        if index % 2 == 0:
+            res.extend(val[::-1])
+        else:
+            res.extend(val)
+    return res
