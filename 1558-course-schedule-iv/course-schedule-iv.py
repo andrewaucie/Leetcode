f707975c25ec119fdef932acddbbe649568e40f7
andrewaucie <aucieandrew@gmail.com>
2024-09-08 16:22:11 -0400

    Time: 592 ms (53.13%) | Memory: 20.5 MB (13.25%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.py
+++ 1558-course-schedule-iv/course-schedule-iv.py
@@ -1,23 +1,33 @@
 class Solution:
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
         # Build reversed graph in topological sorted order
-        graph = {i : set() for i in range(numCourses)}
+        # Store indegree (number of ancestors in top sorted order or # of course that this course is a prereq for)
+        # Store set of prereq courses for each course
+        prereqSet = [set([i]) for i in range(numCourses)]
+        indegree = [0] * numCourses
+        graph = { i : set() for i in range(numCourses)}
         for prereq, course in prerequisites:
-            graph[prereq].add(course)
+            graph[course].add(prereq)
+            indegree[prereq] += 1
         
-
-        def dfs(curr, target, visited):
-            if curr == target:
-                return True
-
-            for nextCourse in graph[curr]:
-                if nextCourse not in visited:
-                    visited.add(nextCourse)
-                    if dfs(nextCourse, target, visited):
-                        return True
-            return False
-
+        # Run BFS on all indegree 0, add to queue if indegree becomes 0 (processed all ancestors and unioned)
+        queue = deque()
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                queue.append(i)
+        while queue:
+            course = queue.popleft()
+            
+            # Check ancestors, decrease indegree, merge sets
+            for neighbor in graph[course]:
+                indegree[neighbor] -= 1
+                prereqSet[neighbor] = prereqSet[neighbor].union(prereqSet[course])
+                if indegree[neighbor] == 0:
+                    queue.append(neighbor)
         res = []
-        for fromCourse, toCourse in queries:
-            res.append(dfs(fromCourse, toCourse, set()))
+        # Check if queries are in corresponding prereq set
+        for prereq, course in queries:
+            res.append((course in prereqSet[prereq]))
         return res
+        
+        
