416a36b4b42f892ee4b55500fc79918104871b9b
andrewaucie <aucieandrew@gmail.com>
2024-09-11 19:53:28 -0400

    Time: 574 ms (58.36%) | Memory: 16.8 MB (82.48%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -3,7 +3,7 @@ class Solution:
         target = sum(nums)
         if target % 2 != 0:
             return False
-        target /= 2
+        target //= 2
         
         # Brute Force
         memo = {}
@@ -15,6 +15,15 @@ class Solution:
             if (i,curr) not in memo:
                 memo[(i,curr)] = bruteforce(i+1, curr + nums[i]) or bruteforce(i+1, curr)
             return memo[(i,curr)]
+
+        # DP
+        dp = [False] * (target+1)
+        dp[0] = True
+        for n in nums:
+            for j in range(target, n-1, -1):
+                dp[j] = dp[j] or dp[j - n]
+        return dp[target]
+
         return bruteforce(0, 0)
     
 # [1, 5, 11, 5] -> [1, 5, 5, 11]
