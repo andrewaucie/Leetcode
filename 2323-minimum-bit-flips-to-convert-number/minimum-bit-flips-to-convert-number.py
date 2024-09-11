045ea8ca4c0867a981331ed1240311c6b901e91f
andrewaucie <aucieandrew@gmail.com>
2024-09-10 22:13:14 -0400

    Time: 36 ms (50.84%) | Memory: 16.6 MB (26.96%) - LeetSync

---
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minBitFlips(self, start: int, goal: int) -> int:
+        xor_result = start ^ goal
+        count = 0
+        
+        # Count the number of 1's in the XOR result
+        while xor_result > 0:
+            count += xor_result & 1  # Check if the least significant bit is 1
+            xor_result >>= 1         # Right shift to check the next bit
+        
+        return count
