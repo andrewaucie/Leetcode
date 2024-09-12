de14aa7de08c18e9881b573c4a59944cb40148a3
andrewaucie <aucieandrew@gmail.com>
2024-09-12 14:22:51 -0400

    Time: 156 ms (75.95%) | Memory: 16.6 MB (86.90%) - LeetSync

--- 494-target-sum/target-sum.py
+++ 494-target-sum/target-sum.py
@@ -4,14 +4,19 @@ class Solution:
         if target > totalSum:
             return 0
         
-        dp = [0] * (totalSum*2 + 1)
-        dp[totalSum - nums[0]] += 1
+        dp = defaultdict(int)
         dp[totalSum + nums[0]] += 1
+        dp[totalSum - nums[0]] += 1
         for i in range(1, len(nums)):
-            nextDP = [0] * (totalSum*2 + 1)
-            for k in range(-totalSum, totalSum+1):
-                if dp[totalSum + k] > 0:
-                    nextDP[totalSum + k + nums[i]] += dp[totalSum + k]
-                    nextDP[totalSum + k - nums[i]] += dp[totalSum + k]
+            nextDP = defaultdict(int)
+            for prevSum in dp:
+                nextDP[prevSum + nums[i]] += dp[prevSum]
+                nextDP[prevSum - nums [i]] += dp[prevSum]
             dp = nextDP
         return dp[totalSum + target]
+        #     for k in range(-totalSum, totalSum+1):
+        #         if dp[totalSum + k] > 0:
+        #             nextDP[totalSum + k + nums[i]] += dp[totalSum + k]
+        #             nextDP[totalSum + k - nums[i]] += dp[totalSum + k]
+        #     dp = nextDP
+        # return dp[totalSum + target]
