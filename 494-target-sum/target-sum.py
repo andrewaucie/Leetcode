f1afaf42f26ea238ccf7ce5063a4b420f3fda6ce
andrewaucie <aucieandrew@gmail.com>
2024-09-12 13:14:24 -0400

    Time: 223 ms (30.98%) | Memory: 37.8 MB (41.48%) - LeetSync

--- 494-target-sum/target-sum.py
+++ 494-target-sum/target-sum.py
@@ -1,13 +1,25 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         
+        # Memoization
+        # memo = {}
+        # def memoization(i, curr):
+        #     if i == len(nums):
+        #         return int(curr == target)
+        #     if (i,curr) not in memo:
+        #         add = memoization(i+1, curr + nums[i])
+        #         subtract = memoization(i+1, curr - nums[i])
+        #         memo[(i,curr)] = add + subtract
+        #     return memo[(i,curr)]
+        
+        # DP
         memo = {}
         def memoization(i, curr):
             if i == len(nums):
-                return int(curr == target)
+                return int(curr == 0)
             if (i,curr) not in memo:
                 add = memoization(i+1, curr + nums[i])
                 subtract = memoization(i+1, curr - nums[i])
                 memo[(i,curr)] = add + subtract
             return memo[(i,curr)]
-        return memoization(0, 0)
+        return memoization(0, target)
