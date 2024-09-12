281afd906a15500cb2ab2a268d7b9766e837eae2
andrewaucie <aucieandrew@gmail.com>
2024-09-11 20:51:48 -0400

    Time: 560 ms (59.91%) | Memory: 16.7 MB (82.48%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -16,14 +16,26 @@ class Solution:
                 memo[(i,curr)] = bruteforce(i+1, curr + nums[i]) or bruteforce(i+1, curr)
             return memo[(i,curr)]
 
-        # DP
+        # 1D DP
         dp = [False] * (target+1)
         dp[0] = True
         for n in nums:
             for j in range(target, n-1, -1):
                 dp[j] = dp[j] or dp[j - n]
+
         return dp[target]
 
+        # 2D DP
+        dp = [[False] * (len(target)+1) for _ in range(len(nums)+1)]
+        dp[0][0] = True
+        for i in range(1, len(nums)+1):
+            curr = nums[i-1]
+            for j in range(target+1):
+                if curr > j:
+                    dp[i][j] = dp[i-1][j]
+                else:
+                    dp[i][j] = dp[i-1][j] or dp[i-1][j-curr]
+        return dp[len(nums)][target]
         return bruteforce(0, 0)
     
 # [1, 5, 11, 5] -> [1, 5, 5, 11]
