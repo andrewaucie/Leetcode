ebce971be92cd4fcf376b81c54a87df7d8c2e0d6
andrewaucie <aucieandrew@gmail.com>
2024-09-12 03:55:36 -0400

    Time: 190 ms (57.16%) | Memory: 26.1 MB (56.64%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -1,23 +1,27 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         # Memoization
-        memo = [[-1] * len(coins) for _ in range(amount+1)]
-        def memoization(i, k):
-            if i == len(coins) or k < 0:
-                return 0
-            if k == 0:
-                return 1
-            if memo[k][i] != -1:
-                return memo[k][i]
-            add = memoization(i, k - coins[i])
-            skip = memoization(i+1, k)
-            memo[k][i] = add + skip
-            return memo[k][i]
-        return memoization(0, amount)
+        # memo = [[-1] * len(coins) for _ in range(amount+1)]
+        # def memoization(i, k):
+        #     if i == len(coins) or k < 0:
+        #         return 0
+        #     if k == 0:
+        #         return 1
+        #     if memo[k][i] != -1:
+        #         return memo[k][i]
+        #     add = memoization(i, k - coins[i])
+        #     skip = memoization(i+1, k)
+        #     memo[k][i] = add + skip
+        #     return memo[k][i]
+        # return memoization(0, amount)
 
         # DP
-        dp = [[-1] * len(coins) for _ in range(amount+1)]
+        dp = [[0] * (amount+1) for _ in range(len(coins)+1)]
+        for i in range(len(coins)+1):
+            dp[i][0] = 1
         for i in range(len(coins)-1, -1, -1):
-            for k in range(amount, -1, -1):
-                dp[k][i] = dp[k][i+1] + dp[k-coins[i]][i]
-        return dp[0][0]
+            for k in range(1, amount+1):
+                dp[i][k] = dp[i+1][k]
+                if k >= coins[i]:
+                    dp[i][k] += dp[i][k - coins[i]]
+        return dp[0][amount]
