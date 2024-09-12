6a646338d3e5c91d58687370d2fc6c2f300ac91d
andrewaucie <aucieandrew@gmail.com>
2024-09-12 18:02:15 -0400

    Time: 289 ms (64.01%) | Memory: 29.9 MB (75.20%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.py
+++ 115-distinct-subsequences/distinct-subsequences.py
@@ -15,5 +15,12 @@ class Solution:
             exclude = memoization(i+1, targetIndex)
             memo[i][targetIndex] = include + exclude
             return memo[i][targetIndex]
-        
         return memoization(0,0)
+        dp = [[0] * (len(t)+1) for _ in range(len(s)+1)]
+        dp[len(s)][len(t)] = 1
+        for i in range(len(s)-1, -1, -1):
+            for j in range(len(t)-1, -1, -1):
+                if s[i] == t[j]:
+                    dp[i][j] = dp[i+1][j+1]
+                dp[i][j] += dp[i+1][j]
+        return dp[0][0]
