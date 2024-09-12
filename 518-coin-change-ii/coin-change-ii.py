56d56beaf98de2e057972f6b76cb270e7d5e698d
andrewaucie <aucieandrew@gmail.com>
2024-09-12 03:44:03 -0400

    Time: 312 ms (36.80%) | Memory: 37.7 MB (32.20%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -1,6 +1,6 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        # Brute Force
+        # Memoization
         memo = [[-1] * len(coins) for _ in range(amount+1)]
         def memoization(i, k):
             if i == len(coins) or k < 0:
@@ -14,3 +14,10 @@ class Solution:
             memo[k][i] = add + skip
             return memo[k][i]
         return memoization(0, amount)
+
+        # DP
+        dp = [[-1] * len(coins) for _ in range(amount+1)]
+        for i in range(len(coins)-1, -1, -1):
+            for k in range(amount, -1, -1):
+                dp[k][i] = dp[k][i+1] + dp[k-coins[i]][i]
+        return dp[0][0]
