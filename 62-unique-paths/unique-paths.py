44aa3c3db00e0fafb9b49b6e108f790722d66de1
andrewaucie <aucieandrew@gmail.com>
2024-09-11 21:52:01 -0400

    Time: 34 ms (67.86%) | Memory: 16.6 MB (15.65%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -2,8 +2,20 @@ class Solution:
     def uniquePaths(self, m: int, n: int) -> int:\
         # Only need row above, and start from left endpoint (=1)
         # Initialize row to 1, since initial row is only one possible path
-        dp = [1] * n
-        for i in range(1, m):
-            for j in range(1, n):
-                dp[j] = dp[j] + dp[j-1]
-        return dp[n-1]
+        # DP
+        # dp = [1] * n
+        # for i in range(1, m):
+        #     for j in range(1, n):
+        #         dp[j] = dp[j] + dp[j-1]
+        # return dp[n-1]
+
+        # Brute Force
+        memo = [[0] * n for _ in range(m)]
+        def bruteforce(i, j):
+            if i == 0 or j == 0:
+                return 1
+            if memo[i][j] == 0:
+                memo[i][j] = bruteforce(i-1, j) + bruteforce(i, j-1)
+            return memo[i][j]
+
+        return bruteforce(m-1, n-1)
