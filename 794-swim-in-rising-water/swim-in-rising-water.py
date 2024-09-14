34a3deb366d06a2cbce1713991de48ce11c02886
andrewaucie <aucieandrew@gmail.com>
2024-09-14 16:27:47 -0400

    Time: 90 ms (58.28%) | Memory: 17.2 MB (67.95%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.py
+++ 794-swim-in-rising-water/swim-in-rising-water.py
@@ -4,7 +4,7 @@ class Solution:
         m = len(grid[0])
         d = {(0,1),(1,0),(0,-1),(-1,0)}
         queue = [(grid[0][0], (0,0))]
-        visited = set()
+        visited = set([0,0])
         while queue:
             currPeak, (i,j) = heapq.heappop(queue)
             if (i,j) == (n-1, m-1):
