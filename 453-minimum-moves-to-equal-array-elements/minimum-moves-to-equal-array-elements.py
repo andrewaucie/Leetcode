4d7906a56eb5735bbc7545f77542e3d2771da75e
andrewaucie <aucieandrew@gmail.com>
2024-09-14 19:02:43 -0400

    Time: 200 ms (49.31%) | Memory: 17.9 MB (87.50%) - LeetSync

--- 453-minimum-moves-to-equal-array-elements/minimum-moves-to-equal-array-elements.py
+++ 453-minimum-moves-to-equal-array-elements/minimum-moves-to-equal-array-elements.py
@@ -1,10 +1,8 @@
 class Solution:
     def minMoves(self, nums: List[int]) -> int:
-        # Align min / max indices
         minNum = min(nums)
         res = 0
         for n in nums:
-            if n > minNum:
-                res += n - minNum
+            res += n - minNum
         return res
         
