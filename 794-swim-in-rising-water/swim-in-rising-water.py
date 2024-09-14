19eea3c4d720c1969e396d6dceaa2859eb308572
andrewaucie <aucieandrew@gmail.com>
2024-09-14 16:24:18 -0400

    Time: 168 ms (7.03%) | Memory: 18.7 MB (5.10%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.py
+++ 794-swim-in-rising-water/swim-in-rising-water.py
@@ -2,7 +2,7 @@ class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
         n = len(grid)
         m = len(grid[0])
-        
+        d = {(0,1),(1,0),(0,-1),(-1,0)}
         queue = [(grid[0][0], (0,0))]
         heapq.heapify(queue)
         visited = set()
@@ -12,7 +12,7 @@ class Solution:
             peak = max(peak, currPeak)
             if (i,j) == (n-1, m-1):
                 return peak
-            for x,y in {(0,1),(1,0),(0,-1),(-1,0)}:
+            for x,y in d:
                 if (0 <= i+x < n and 0 <= j+y < m) and (i,j,x,y) not in visited:
                     visited.add((i,j,x,y))
                     heapq.heappush(queue, (max(grid[i+x][j+y], currPeak), (i+x,j+y)))
