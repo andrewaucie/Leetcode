7a9c3bab491240a9dd1896d197db04f3ed2a4b28
andrewaucie <aucieandrew@gmail.com>
2024-09-14 00:04:35 -0400

    Time: 35 ms (58.81%) | Memory: 16.6 MB (34.85%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.py
+++ 678-valid-parenthesis-string/valid-parenthesis-string.py
@@ -1,34 +1,24 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
-        paranthesis = 0
-        star = 0
+        paranthesis = star = 0
         for p in s:
             if p == "(":
                 paranthesis += 1
             elif p == ")":
                 if paranthesis == 0:
-                    if star > 0:
-                        star -= 1
-                        paranthesis += 1
-                    else:
-                        return False
+                    return False
                 paranthesis -= 1
             elif p == "*":
-                star += 1
-        
-        paranthesis = 0
-        star = 0
+                paranthesis += 1
+
+        paranthesis = star = 0
         for p in reversed(s):
             if p == ")":
                 paranthesis += 1
             elif p == "(":
                 if paranthesis == 0:
-                    if star > 0:
-                        star -= 1
-                        paranthesis += 1
-                    else:
-                        return False
+                    return False
                 paranthesis -= 1
             elif p == "*":
-                star += 1
+                paranthesis += 1
         return True
