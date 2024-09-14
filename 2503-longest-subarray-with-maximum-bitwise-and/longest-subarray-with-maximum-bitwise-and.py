fb8b3df1cbbbebb63671ed57931c962d63eac6d9
andrewaucie <aucieandrew@gmail.com>
2024-09-14 13:10:47 -0400

    Time: 659 ms (50.55%) | Memory: 30.9 MB (85.09%) - LeetSync

--- 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.py
+++ 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.py
@@ -2,11 +2,15 @@ class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
         longest = 0
         curr = 0
-        maxNum = max(nums)
+        currMax = nums[0]
         for n in nums:
-            if n == maxNum:
+            if n == currMax:
                 curr += 1
                 longest = max(longest, curr)
+            elif n > currMax:
+                currMax = n
+                curr = 1
+                longest = 1
             else:
                 curr = 0
         return longest
