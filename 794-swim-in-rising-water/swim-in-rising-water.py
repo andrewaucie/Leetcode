8ad0a369e4d660ed099e50623af30e5313d47a79
andrewaucie <aucieandrew@gmail.com>
2024-09-14 16:24:56 -0400

    Time: 168 ms (7.03%) | Memory: 18.5 MB (5.10%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.py
+++ 794-swim-in-rising-water/swim-in-rising-water.py
@@ -6,14 +6,11 @@ class Solution:
         queue = [(grid[0][0], (0,0))]
         heapq.heapify(queue)
         visited = set()
-        peak = grid[0][0]
         while queue:
             currPeak, (i,j) = heapq.heappop(queue)
-            peak = max(peak, currPeak)
             if (i,j) == (n-1, m-1):
-                return peak
+                return currPeak
             for x,y in d:
                 if (0 <= i+x < n and 0 <= j+y < m) and (i,j,x,y) not in visited:
                     visited.add((i,j,x,y))
                     heapq.heappush(queue, (max(grid[i+x][j+y], currPeak), (i+x,j+y)))
-        return peak
