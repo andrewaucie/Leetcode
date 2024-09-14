23e40aa8787c547d28c52fb5b8455f26dc09360d
andrewaucie <aucieandrew@gmail.com>
2024-09-14 00:02:52 -0400

    Time: 34 ms (66.41%) | Memory: 16.5 MB (34.85%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.py
+++ 678-valid-parenthesis-string/valid-parenthesis-string.py
@@ -1,20 +1,34 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
-        memo = {}
-        def backtrack(i, p):
-            if p < 0:
-                return False
-            if i == len(s):
-                return p == 0
-            if (i, p) in memo:
-                return memo[(i, p)]
-            left, right, star = False, False, False
-            if s[i] == "(":
-                left = backtrack(i+1, p+1)
-            elif s[i] == ")":
-                right = backtrack(i+1, p-1)
-            else:
-                star = backtrack(i+1, p+1) or backtrack(i+1, p-1) or backtrack(i+1, p)
-            memo[(i,p)] = left or right or star
-            return left or right or star
-        return backtrack(0,0)
+        paranthesis = 0
+        star = 0
+        for p in s:
+            if p == "(":
+                paranthesis += 1
+            elif p == ")":
+                if paranthesis == 0:
+                    if star > 0:
+                        star -= 1
+                        paranthesis += 1
+                    else:
+                        return False
+                paranthesis -= 1
+            elif p == "*":
+                star += 1
+        
+        paranthesis = 0
+        star = 0
+        for p in reversed(s):
+            if p == ")":
+                paranthesis += 1
+            elif p == "(":
+                if paranthesis == 0:
+                    if star > 0:
+                        star -= 1
+                        paranthesis += 1
+                    else:
+                        return False
+                paranthesis -= 1
+            elif p == "*":
+                star += 1
+        return True
