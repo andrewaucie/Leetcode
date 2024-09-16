b6b119e65198bf4bf554aaa9a0dd905749b0591b
andrewaucie <aucieandrew@gmail.com>
2024-09-15 20:17:35 -0400

    Time: 44 ms (92.36%) | Memory: 16.5 MB (98.29%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.py
+++ 44-wildcard-matching/wildcard-matching.py
@@ -1,22 +1,39 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        memo = {}
-        def backtrack(string, pattern):
-            if string == -1 and pattern == -1:
-                return True
-            if pattern == -1:
+        s_len, p_len = len(s), len(p)
+        s_idx = p_idx = 0
+        star_idx = s_tmp_idx = -1
+
+        while s_idx < s_len:
+            # If the pattern caracter = string character
+            # or pattern character = '?'
+            if p_idx < p_len and p[p_idx] in ["?", s[s_idx]]:
+                s_idx += 1
+                p_idx += 1
+
+            # If pattern character = '*'
+            elif p_idx < p_len and p[p_idx] == "*":
+                # Check the situation
+                # when '*' matches no characters
+                star_idx = p_idx
+                s_tmp_idx = s_idx
+                p_idx += 1
+
+            # If pattern character != string character
+            # or pattern is used up
+            # and there was no '*' character in pattern
+            elif star_idx == -1:
                 return False
-            if (string, pattern) in memo:
-                return memo[(string, pattern)]
-            if string == -1:
-                memo[(string, pattern)] = (p[pattern] == "*" and backtrack(string, pattern-1))
-                return memo[(string, pattern)]
-            if s[string] == p[pattern] or p[pattern] == '?':
-                memo[(string, pattern)] = backtrack(string-1, pattern-1)
-            elif p[pattern] == '*':
-                memo[(string, pattern)] = backtrack(string-1, pattern) or backtrack(string, pattern-1)
+
+            # If pattern character != string character
+            # or pattern is used up
+            # and there was '*' character in pattern before
             else:
-                memo[(string, pattern)] = False
-            return memo[(string, pattern)]
-        return backtrack(len(s)-1, len(p)-1)
-                
+                # Backtrack: check the situation
+                # when '*' matches one more character
+                p_idx = star_idx + 1
+                s_idx = s_tmp_idx + 1
+                s_tmp_idx = s_idx
+
+        # The remaining characters in the pattern should all be '*' characters
+        return all(p[i] == "*" for i in range(p_idx, p_len))
