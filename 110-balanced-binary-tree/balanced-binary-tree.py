84e0312c2fd14117cc59770ce7c1136ffa07e3f4
andrewaucie <aucieandrew@gmail.com>
2024-09-18 13:33:26 -0400

    Time: 55 ms (10.26%) | Memory: 17.6 MB (93.93%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.py
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -1,20 +1,20 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
-    def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        
-        def balanced(node):
-            if not node:
-                return [True, 0]
-            leftBalanced, leftDepth = balanced(node.left)
-            if not leftBalanced:
-                return [False, 0]
-            rightBalanced, rightDepth = balanced(node.right)
-            if not rightBalanced:
-                return [False, 0]
-            return [abs(leftDepth-rightDepth) <= 1, max(leftDepth, rightDepth) + 1]
-        return balanced(root)[0]
+    # Compute the tree's height via recursion
+    def height(self, root: TreeNode) -> int:
+        # An empty tree has height -1
+        if not root:
+            return 0
+        return 1 + max(self.height(root.left), self.height(root.right))
+
+    def isBalanced(self, root: TreeNode) -> bool:
+        # An empty tree satisfies the definition of a balanced tree
+        if not root:
+            return True
+
+        # Check if subtrees have height within 1. If they do, check if the
+        # subtrees are balanced
+        return (
+            abs(self.height(root.left) - self.height(root.right)) < 2
+            and self.isBalanced(root.left)
+            and self.isBalanced(root.right)
+        )
