c4bf8475d165dfe77da8daf942bd98f03dbd19e9
andrewaucie <aucieandrew@gmail.com>
2024-09-19 02:25:56 -0400

    Time: 1634 ms (31.49%) | Memory: 41.8 MB (29.91%) - LeetSync

--- 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.py
+++ 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.py
@@ -1,18 +1,12 @@
 class Solution:
     def findLength(self, nums1: List[int], nums2: List[int]) -> int:
-        memo = [[-1] * (len(nums2)+1) for _ in range(len(nums1)+1)]
-        def backtrack(i, j):
-            if i == 0 or j == 0:
-                return 0
-            if memo[i][j] != -1:
-                return memo[i][j]
-            matched = 0
-            if nums1[i-1] == nums2[j-1]:
-                matched = backtrack(i-1, j-1) + 1
-            memo[i][j] = matched
-            return memo[i][j]
+        dp = [[0] * (len(nums2)+1) for _ in range(len(nums1)+1)]
         maxLen = 0
         for i in range(1, len(nums1)+1):
             for j in range(1, len(nums2)+1):
-                maxLen = max(maxLen, backtrack(i,j))
+                if nums1[i-1] == nums2[j-1]:
+                    dp[i][j] = dp[i-1][j-1] + 1
+                    maxLen = max(maxLen, dp[i][j])
+                else:
+                    dp[i][j] = 0
         return maxLen
