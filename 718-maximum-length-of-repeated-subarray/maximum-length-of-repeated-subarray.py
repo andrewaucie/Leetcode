bddf8a002f93b0cd0c522fc774fc96de1fb8b88a
andrewaucie <aucieandrew@gmail.com>
2024-09-19 02:23:46 -0400

    Time: 3443 ms (5.03%) | Memory: 119.7 MB (5.01%) - LeetSync

--- 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.py
+++ 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.py
@@ -1,22 +1,18 @@
 class Solution:
     def findLength(self, nums1: List[int], nums2: List[int]) -> int:
-        m,n = len(nums1), len(nums2)
-        memo = [[-1] * (n+1) for _ in range(m+1)]
-
-        def longest_common_subarray(i,j):
+        memo = [[-1] * (len(nums2)+1) for _ in range(len(nums1)+1)]
+        def backtrack(i, j):
             if i == 0 or j == 0:
                 return 0
             if memo[i][j] != -1:
                 return memo[i][j]
-            
-            res = 0
+            matched = 0
             if nums1[i-1] == nums2[j-1]:
-                res = 1 + longest_common_subarray(i-1,j-1)
-            memo[i][j] = res
+                matched = backtrack(i-1, j-1) + 1
+            memo[i][j] = matched
             return memo[i][j]
-
-        max_length = 0
-        for i in range(1,m+1):
-            for j in range(1,n+1):
-                max_length = max(max_length, longest_common_subarray(i,j))
-        return max_length
+        maxLen = 0
+        for i in range(1, len(nums1)+1):
+            for j in range(1, len(nums2)+1):
+                maxLen = max(maxLen, backtrack(i,j))
+        return maxLen
