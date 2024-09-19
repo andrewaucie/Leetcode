fc9d1a616327cd47ef9aa4e56906d6ecafb843cb
andrewaucie <aucieandrew@gmail.com>
2024-09-18 22:38:58 -0400

    Time: 31 ms (90.48%) | Memory: 16.8 MB (27.58%) - LeetSync

--- 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.py
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.py
@@ -1,30 +1,40 @@
 class Solution:
     def diffWaysToCompute(self, expression: str) -> List[int]:
-        memo = {}
-        
-        def helper(expr):
-            if expr in memo:
-                return memo[expr]
-            
-            result = []
-            for i, char in enumerate(expr):
-                if char in "+-*":
-                    left = helper(expr[:i])
-                    right = helper(expr[i+1:])
-                    
-                    for l in left:
-                        for r in right:
-                            if char == '+':
-                                result.append(l + r)
-                            elif char == '-':
-                                result.append(l - r)
+        nums, ops = [], []
+        i = 0
+        while i < len(expression):
+            if expression[i].isdigit():
+                num = 0
+                while i < len(expression) and expression[i].isdigit():
+                    num = num * 10 + int(expression[i])
+                    i += 1
+                nums.append(num)
+            else:
+                ops.append(expression[i])
+                i += 1
+
+        n = len(nums)
+        dp = [[[] for _ in range(n)] for _ in range(n)]
+
+        for i in range(n):
+            dp[i][i] = [nums[i]]
+
+        for length in range(2, n + 1):
+            for i in range(n - length + 1):
+                j = i + length - 1
+                dp[i][j] = []
+                for k in range(i, j):
+                    left_results = dp[i][k]
+                    right_results = dp[k + 1][j]
+                    operator = ops[k]
+
+                    for left in left_results:
+                        for right in right_results:
+                            if operator == '+':
+                                dp[i][j].append(left + right)
+                            elif operator == '-':
+                                dp[i][j].append(left - right)
                             else:
-                                result.append(l * r)
-            
-            if not result:
-                result.append(int(expr))
-            
-            memo[expr] = result
-            return result
-        
-        return helper(expression)
+                                dp[i][j].append(left * right)
+
+        return dp[0][n - 1]
