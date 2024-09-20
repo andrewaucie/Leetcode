2f737a5f315948300f3632b7ce22b885121c56e4
andrewaucie <aucieandrew@gmail.com>
2024-09-20 00:38:35 -0400

    Time: 1385 ms (79.37%) | Memory: 74.1 MB (52.24%) - LeetSync

--- 2409-number-of-increasing-paths-in-a-grid/number-of-increasing-paths-in-a-grid.py
+++ 2409-number-of-increasing-paths-in-a-grid/number-of-increasing-paths-in-a-grid.py
@@ -1,15 +1,15 @@
 class Solution:
     def countPaths(self, grid: List[List[int]]) -> int:
         
-        memo = {}
+        memo = [[-1] * len(grid[0]) for _ in range(len(grid))]
         def backtrack(i,j):
-            if (i,j) in memo:
-                return memo[(i,j)]
+            if memo[i][j] != -1:
+                return memo[i][j]
             res = 1
             for dx, dy in {(0,1),(1,0),(0,-1),(-1,0)}:
                 if 0 <= i+dx < len(grid) and 0 <= j+dy < len(grid[0]) and grid[i+dx][j+dy] > grid[i][j]:
                    res += backtrack(i+dx, j+dy)
-            memo[(i,j)] = res
+            memo[i][j] = res
             return res
         res = 0
         for i in range(len(grid)):
