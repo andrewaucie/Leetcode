98dd855dda6866965cbf13f18206ce94c97580d4
andrewaucie <aucieandrew@gmail.com>
2024-09-19 22:22:37 -0400

    Time: 679 ms (19.65%) | Memory: 16.7 MB (91.91%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.py
+++ 214-shortest-palindrome/shortest-palindrome.py
@@ -1,31 +1,8 @@
 class Solution:
     def shortestPalindrome(self, s: str) -> str:
-        # Reverse the original string
-        reversed_string = s[::-1]
-
-        # Combine the original and reversed strings with a separator
-        combined_string = s + "#" + reversed_string
-
-        # Build the prefix table for the combined string
-        prefix_table = self._build_prefix_table(combined_string)
-
-        # Get the length of the longest palindromic prefix
-        palindrome_length = prefix_table[-1]
-
-        # Construct the shortest palindrome by appending the reverse of the suffix
-        suffix = reversed_string[: len(s) - palindrome_length]
-        return suffix + s
-
-    # Helper function to build the KMP prefix table
-    def _build_prefix_table(self, s: str) -> list:
-        prefix_table = [0] * len(s)
-        length = 0
-
-        # Build the table by comparing characters
-        for i in range(1, len(s)):
-            if length > 0 and s[i] != s[length]:
-                length = prefix_table[length - 1]
-            if s[i] == s[length]:
-                length += 1
-            prefix_table[i] = length
-        return prefix_table
+        begin = ''
+        i = -1
+        while (begin + s) != (begin + s)[::-1]:
+            begin += s[i]
+            i -= 1
+        return begin + s
