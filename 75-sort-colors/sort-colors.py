28f828282218f842463bdb8b3bfcda3d4c795081
andrewaucie <aucieandrew@gmail.com>
2024-09-19 20:11:44 -0400

    Time: 35 ms (74.14%) | Memory: 16.4 MB (93.30%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -3,14 +3,14 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        count = Counter(nums)
+        last0, first2 = 0, len(nums) - 1
         i = 0
-        while i < count[0]:
-            nums[i] = 0
-            i += 1
-        while i < count[0] + count[1]:
-            nums[i] = 1
-            i += 1
-        while i < len(nums):
-            nums[i] = 2
+        while i <= first2:
+            while i >= last0 and i <= first2 and (nums[i] == 0 or nums[i] == 2):
+                if nums[i] == 0:
+                    nums[last0], nums[i] = nums[i], nums[last0]
+                    last0 += 1
+                elif nums[i] == 2:
+                    nums[first2], nums[i] = nums[i], nums[first2]
+                    first2 -= 1
             i += 1
