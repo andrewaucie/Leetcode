8457de475992caa0d31183fe3f883d822dfb1af1
andrewaucie <aucieandrew@gmail.com>
2024-09-20 21:57:01 -0400

    Time: 48 ms (89.63%) | Memory: 20.3 MB (33.07%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,22 +7,11 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        pathP = self.createPath(root, p, [root])
-        pathQ = self.createPath(root, q, [root])
-        i = 0
-        while i < len(pathP)-1 and i < len(pathQ)-1 and pathP[i+1] == pathQ[i+1]:
-            i += 1
-        return pathP[i]
-            
-    def createPath(self, node, target, path):
-        if not node:
-            return [None]
-        if node.val == target.val:
-            return path + [target]
-        left = self.createPath(node.left, target, path + [node.left])
-        right = self.createPath(node.right, target, path + [node.right])
-        if left[-1] == target:
-            return left
-        elif right[-1] == target:
-            return right
-        return [None]
+        node = root
+        while node:
+            if p.val > node.val and q.val > node.val:
+                node = node.right
+            elif p.val < node.val and q.val < node.val:
+                node = node.left
+            else:
+                return node
