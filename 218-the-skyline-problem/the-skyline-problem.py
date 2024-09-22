3949eec6a89bf20b0ca31240f4cc22d014f5c1b5
andrewaucie <aucieandrew@gmail.com>
2024-09-22 17:40:58 -0400

    Time: 116 ms (33.11%) | Memory: 22.6 MB (30.05%) - LeetSync

--- 218-the-skyline-problem/the-skyline-problem.py
+++ 218-the-skyline-problem/the-skyline-problem.py
@@ -1,33 +1,46 @@
 class Solution:
     def getSkyline(self, buildings: List[List[int]]) -> List[List[int]]:
-        # Sort the unique positions of all the edges.
-        positions = sorted(list(set([x for building in buildings for x in building[:2]])))
+        edges = []
+        for i, build in enumerate(buildings):
+            edges.append([build[0], i])
+            edges.append([build[1], i])
+
+        edges.sort()
+        print(edges)
+        live, answer = [], []
+        idx = 0
         
-        # Hast table 'edge_index_map' to record every {position : index} pairs in edges.
-        edge_index_map = {x : i for i, x in enumerate(positions)}
-
-        # Initialize 'heights' to record maximum height at each index.
-        heights = [0] * len(positions)
+        while idx < len(edges):
+            
+            # Since we might have multiple edges at same x,
+            # Let the 'curr_x' be the current position.
+            curr_x = edges[idx][0]
+            
+            # While we are handling the edges at 'curr_x':
+            while idx < len(edges) and edges[idx][0] == curr_x:
+                # The index 'b' of this building in 'buildings'
+                b = edges[idx][1]
+                
+                # If this is a left edge of building 'b', we
+                # add (height, right) of building 'b' to 'live'.
+                if buildings[b][0] == curr_x:
+                    right = buildings[b][1]
+                    height = buildings[b][2]
+                    heapq.heappush(live, [-height, right])
+                    
+                # If the tallest live building has been passed,
+                # we remove it from 'live'.
+                while live and live[0][1] <= curr_x:
+                    heapq.heappop(live)
+                idx += 1
+            
+            # Get the maximum height from 'live'.
+            max_height = -live[0][0] if live else 0
+            
+            # If the height changes at this curr_x, we add this
+            # skyline key point [curr_x, max_height] to 'answer'.
+            if not answer or max_height != answer[-1][1]:
+                answer.append([curr_x, max_height])
         
-        # Iterate over all the buildings.
-        for left, right, height in buildings:
-            # For each building, find the indexes of its left
-            # and right edges.
-            left_idx = edge_index_map[left]
-            right_idx = edge_index_map[right]
-
-            # Update the maximum height within the range [left_idx, right_idx)
-            for i in range(left_idx, right_idx):
-                heights[i] = max(heights[i], height)
-
-        answer = []
-
-        # Iterate over 'heights'.
-        for i in range(len(heights)):
-            curr_height = heights[i]
-            curr_x = positions[i]
-
-            # Add all the positions where the height changes to 'answer'.
-            if not answer or answer[-1][1] != curr_height:
-                answer.append([curr_x, curr_height])
+        # Return 'answer' as the skyline.
         return answer
