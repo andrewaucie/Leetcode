6bc82ba3425c85b6b6a82032daa42b925f9acb15
andrewaucie <aucieandrew@gmail.com>
2024-09-22 17:41:27 -0400

    Time: 109 ms (44.28%) | Memory: 22.7 MB (25.40%) - LeetSync

--- 218-the-skyline-problem/the-skyline-problem.py
+++ 218-the-skyline-problem/the-skyline-problem.py
@@ -11,36 +11,23 @@ class Solution:
         idx = 0
         
         while idx < len(edges):
-            
-            # Since we might have multiple edges at same x,
-            # Let the 'curr_x' be the current position.
             curr_x = edges[idx][0]
             
-            # While we are handling the edges at 'curr_x':
             while idx < len(edges) and edges[idx][0] == curr_x:
-                # The index 'b' of this building in 'buildings'
                 b = edges[idx][1]
-                
-                # If this is a left edge of building 'b', we
-                # add (height, right) of building 'b' to 'live'.
+
                 if buildings[b][0] == curr_x:
                     right = buildings[b][1]
                     height = buildings[b][2]
                     heapq.heappush(live, [-height, right])
-                    
-                # If the tallest live building has been passed,
-                # we remove it from 'live'.
+
                 while live and live[0][1] <= curr_x:
                     heapq.heappop(live)
                 idx += 1
             
-            # Get the maximum height from 'live'.
             max_height = -live[0][0] if live else 0
             
-            # If the height changes at this curr_x, we add this
-            # skyline key point [curr_x, max_height] to 'answer'.
             if not answer or max_height != answer[-1][1]:
                 answer.append([curr_x, max_height])
         
-        # Return 'answer' as the skyline.
         return answer
