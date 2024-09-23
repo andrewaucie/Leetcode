9f3f1184665a49987fd6e4f78354c75ae553a7c4
andrewaucie <aucieandrew@gmail.com>
2024-09-22 22:12:44 -0400

    Time: 891 ms (18.20%) | Memory: 17.1 MB (46.34%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.py
+++ 1129-longest-string-chain/longest-string-chain.py
@@ -1,16 +1,27 @@
 class Solution:
+    def compare(self,suc,pre):
+        if len(suc)!=len(pre)+1:
+            return False
+        first=second=0
+        while first<len(suc):
+            if second<len(pre) and suc[first]==pre[second]:
+                first+=1
+                second+=1
+            else:
+                first+=1
+        if first==len(suc) and second==len(pre):
+            return True
+        else:
+            return False
+        
     def longestStrChain(self, words: List[str]) -> int:
-        words.sort(key=len)
-    
-        dp = {}
-        maxChainLength = 0
-        
-        for word in words:
-            dp[word] = 1
-            for i in range(len(word)):
-                pred = word[:i] + word[i+1:]
-                if pred in dp:
-                    dp[word] = max(dp[word], dp[pred] + 1)
-            maxChainLength = max(maxChainLength, dp[word])
-        
-        return maxChainLength
+        words=sorted(words,key=len)
+        n=len(words)           
+        dp=[1]*n
+        maxi=1
+        for i in range(1,n):
+            for prev in range(i): 
+                if self.compare(words[i],words[prev]) and dp[prev]+1>dp[i]:
+                    dp[i]=dp[prev]+1
+            maxi=max(maxi,dp[i])
+        return maxi
