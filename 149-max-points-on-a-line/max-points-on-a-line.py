6008ae81d0ff77153c75912fd947af1f7698c46c
andrewaucie <aucieandrew@gmail.com>
2024-09-23 01:06:59 -0400

    Time: 67 ms (75.12%) | Memory: 16.7 MB (74.77%) - LeetSync

--- 149-max-points-on-a-line/max-points-on-a-line.py
+++ 149-max-points-on-a-line/max-points-on-a-line.py
@@ -1,16 +1,15 @@
 class Solution:
     def maxPoints(self, points: List[List[int]]) -> int:
-        slopeMap = defaultdict(set)
+        res = 1
         for i in range(len(points)):
+            x1,y1 = points[i]
+            slopeMap = defaultdict(int)
             for j in range(i+1, len(points)):
-                x1,y1 = points[i]
                 x2,y2 = points[j]
                 if x1 == x2:
                     slope = float('inf')
-                    b = x1
                 else:
                     slope = (y1-y2) / (x1-x2)
-                    b = y1 - slope * x1
-                slopeMap[(slope,b)].add(i)
-                slopeMap[(slope,b)].add(j)
-        return len(max(slopeMap.values(), key=len)) if slopeMap else 1
+                slopeMap[slope] += 1
+                res = max(res, slopeMap[slope] + 1)
+        return res
