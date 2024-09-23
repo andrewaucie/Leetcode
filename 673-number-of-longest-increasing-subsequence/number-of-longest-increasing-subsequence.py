a290a13f48e483379ddaf094dbf2c51019e413a2
andrewaucie <aucieandrew@gmail.com>
2024-09-23 00:14:57 -0400

    Time: 506 ms (90.11%) | Memory: 17.2 MB (8.58%) - LeetSync

--- 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.py
+++ 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.py
@@ -1,33 +1,23 @@
 class Solution:
-    def findNumberOfLIS(self, nums):
-        n = len(nums)
-        length = [0] * n
-        count = [0] * n
+    def findNumberOfLIS(self, nums: List[int]) -> int:
+        ct = defaultdict(list)
+        ct[0] = [1,1]
+        length,longest,ans = len(nums),1,1
 
-        def calculate_dp(i):
-            if length[i] != 0:
-                return
-
-            length[i] = 1
-            count[i] = 1
-
-            for j in range(i):
-                if nums[j] < nums[i]:
-                    calculate_dp(j)
-                    if length[j] + 1 > length[i]:
-                        length[i] = length[j] + 1
-                        count[i] = 0
-                    if length[j] + 1 == length[i]:
-                        count[i] += count[j]
-
-        max_length = 0
-        result = 0
-        for i in range(n):
-            calculate_dp(i)
-            max_length = max(max_length, length[i])
-
-        for i in range(n):
-            if length[i] == max_length:
-                result += count[i]
-
-        return result
+        for right in range(1,length):
+            map,cur_longest = defaultdict(lambda:1),1
+            for left in range(right-1,-1,-1):
+                if nums[right] > nums[left] and ct[left][0]+1>=cur_longest:
+                    temp_long,temp_ct = ct[left]
+                    cur_longest = temp_long+1
+                    map[cur_longest] += temp_ct
+            if map:
+                map[cur_longest]-=1
+                ct[right] = [cur_longest,map[cur_longest]]
+            else:
+                ct[right] = ct[0]
+            if cur_longest > longest:
+                longest,ans = cur_longest,map[cur_longest]
+            elif cur_longest == longest:
+                ans += map[cur_longest]
+        return ans
