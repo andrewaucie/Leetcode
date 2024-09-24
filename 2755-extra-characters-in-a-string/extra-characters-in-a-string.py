ae44ff38e9aedfe53f832004460e7c5368cf5569
andrewaucie <aucieandrew@gmail.com>
2024-09-23 21:03:03 -0400

    Time: 204 ms (53.31%) | Memory: 16.6 MB (83.10%) - LeetSync

--- 2755-extra-characters-in-a-string/extra-characters-in-a-string.py
+++ 2755-extra-characters-in-a-string/extra-characters-in-a-string.py
@@ -1,17 +1,11 @@
 class Solution:
     def minExtraChar(self, s: str, dictionary: List[str]) -> int:
         dictionary = set(dictionary)
-        memo = [-1] * len(s)
-        def backtrack(i):
-            if i == len(s):
-                return 0
-            if memo[i] != -1:
-                return memo[i]
-            minExtra = backtrack(i+1) + 1
+        dp = [0] * (len(s)+1)
+        for i in range(len(s)-1, -1, -1):
+            dp[i] = dp[i+1] + 1
             for j in range(i, len(s)):
                 curr = s[i:j+1]
                 if curr in dictionary:
-                    minExtra = min(minExtra, backtrack(j+1))
-            memo[i] = minExtra
-            return memo[i]
-        return backtrack(0)
+                    dp[i] = min(dp[i], dp[j+1])
+        return dp[0]
