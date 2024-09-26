0973c965db0dd597282fcb745feb82f4e71e5232
andrewaucie <aucieandrew@gmail.com>
2024-09-26 16:02:06 -0400

    Time: 551 ms (16.51%) | Memory: 17.5 MB (35.52%) - LeetSync

--- 729-my-calendar-i/my-calendar-i.py
+++ 729-my-calendar-i/my-calendar-i.py
@@ -1,32 +1,10 @@
 class MyCalendar:
-
     def __init__(self):
-        self.d = {}
-        self.count = 0
-        
-    '''
-    O(nlogN) sorting
-    outer loop * remove => O(N) * O(N) => O(N^2)
-    '''
-    
-    def checkOverlaps(self, newKey):
-        bookings = list(self.d.values())
-        lst = []
-        for start, end in bookings:
-            lst.append((start, +1)) # one booking added at startingtime = start
-            lst.append((end, -1)) # booking ends at timestamp = end
-            
-        lst.sort()
-        overlaps = 0
-        for book in lst:
-            overlaps += book[1]
-            if overlaps > 1:
-                del self.d[newKey] # remove the booking that is causing problems (triple overlap)
-                return False
-        return True
-    
-    
+        self.calendar = []
+
     def book(self, start: int, end: int) -> bool:
-        self.count += 1
-        self.d[self.count] = (start,end)
-        return self.checkOverlaps(self.count)
+        for x in self.calendar:
+            if (start < x[0] and end > x[0]) or (start == x[0]) or (start > x[0] and start < x[1]):
+                return False
+        self.calendar.append([start, end])
+        return True
