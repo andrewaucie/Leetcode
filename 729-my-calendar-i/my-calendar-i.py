87a0d9552b9e8bef558d8f9f7e690fd725f05321
andrewaucie <aucieandrew@gmail.com>
2024-09-26 16:23:51 -0400

    Time: 140 ms (99.75%) | Memory: 17.5 MB (35.52%) - LeetSync

--- 729-my-calendar-i/my-calendar-i.py
+++ 729-my-calendar-i/my-calendar-i.py
@@ -1,17 +1,15 @@
 class MyCalendar:
 
     def __init__(self):
-        self.L = []
-        self.m = {}
-        
-    def book(self, start: int, end: int) -> bool:
-        idx = bisect.bisect_right(self.L, start)
-        if idx == len(self.L) or self.m[self.L[idx]] >= end:
-            bisect.insort(self.L, end)
-            self.m[end] = start
-            return True
+        self.l = [(-1, -1), (10**9+1, 10**9+1)]
 
-        return False
+    def book(self, start: int, end: int) -> bool:
+        index = bisect.bisect_left(self.l, (start, end))
+        if self.l[index - 1][1] > start or self.l[index][0] < end:
+            return False
+        self.l.insert(index, (start, end))
+        return True
+
 
 # Your MyCalendar object will be instantiated and called as such:
 # obj = MyCalendar()
