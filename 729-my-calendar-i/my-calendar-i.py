0669adeac8c768d8a39bfdf4673f8e50a89842fc
andrewaucie <aucieandrew@gmail.com>
2024-09-26 16:00:55 -0400

    Time: 3344 ms (5.04%) | Memory: 17.5 MB (35.52%) - LeetSync

--- 729-my-calendar-i/my-calendar-i.py
+++ 729-my-calendar-i/my-calendar-i.py
@@ -1,28 +1,32 @@
 class MyCalendar:
 
     def __init__(self):
-        self.events = []
-
-
-    def book(self, start: int, end: int) -> bool:
-        # [(pos, "start"), (pos, "end"), etc]
-        heap = list(self.events)
-        heapq.heappush(heap, (start, "start"))
-        heapq.heappush(heap, (end, "end"))
-        count = 0
-        while heap:
-            time, eventType = heapq.heappop(heap)
-            if eventType == "start":
-                count += 1
-            else:
-                count -= 1
-            if count > 1:
+        self.d = {}
+        self.count = 0
+        
+    '''
+    O(nlogN) sorting
+    outer loop * remove => O(N) * O(N) => O(N^2)
+    '''
+    
+    def checkOverlaps(self, newKey):
+        bookings = list(self.d.values())
+        lst = []
+        for start, end in bookings:
+            lst.append((start, +1)) # one booking added at startingtime = start
+            lst.append((end, -1)) # booking ends at timestamp = end
+            
+        lst.sort()
+        overlaps = 0
+        for book in lst:
+            overlaps += book[1]
+            if overlaps > 1:
+                del self.d[newKey] # remove the booking that is causing problems (triple overlap)
                 return False
-        heappush(self.events, (start, "start"))
-        heappush(self.events, (end, "end"))
         return True
-
-
-# Your MyCalendar object will be instantiated and called as such:
-# obj = MyCalendar()
-# param_1 = obj.book(start,end)
+    
+    
+    def book(self, start: int, end: int) -> bool:
+        self.count += 1
+        self.d[self.count] = (start,end)
+        return self.checkOverlaps(self.count)
