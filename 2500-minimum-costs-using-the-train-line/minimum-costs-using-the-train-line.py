b0917dd5552e722b56f69a857948f47381facc6c
andrewaucie <aucieandrew@gmail.com>
2024-09-28 19:23:28 -0400

    Time: 1110 ms (13.85%) | Memory: 48.9 MB (15.44%) - LeetSync

--- 2500-minimum-costs-using-the-train-line/minimum-costs-using-the-train-line.py
+++ 2500-minimum-costs-using-the-train-line/minimum-costs-using-the-train-line.py
@@ -1,12 +1,41 @@
 class Solution:
     def minimumCosts(self, regular: List[int], express: List[int], expressCost: int) -> List[int]:
-        minRegular = 0
-        minExpress = expressCost
-        res = []
-
-        for i in range(len(regular)):
-            minRegular = min(minRegular + regular[i], minExpress + express[i])
-            minExpress = min(minRegular + expressCost, minExpress + express[i])
-            res.append(min(minRegular, minExpress))
         
+        # memo = {}
+        # def memoization(stop, exp, dest):
+        #     if (stop,exp,dest) in memo:
+        #         return memo[(stop,exp,dest)]
+        #     if stop == dest:
+        #         return 0
+        #     if exp:
+        #         stayCost = express[stop]
+        #         switchCost = 0
+        #     else:
+        #         stayCost = regular[stop]
+        #         switchCost = expressCost
+        #     stay = memoization(stop+1, exp, dest)
+        #     switch = switchCost + memoization(stop+1, not exp, dest)
+        #     memo[(stop,exp,dest)] = stayCost + min(stay, switch)
+        #     return memo[(stop,exp,dest)]
+        
+        # res = []
+        # for i in range(1, len(regular)+1):
+        #     cost = min(memoization(0, False, i), memoization(0, True, i) + expressCost)
+        #     res.append(cost)
+        
+        # dp
+        res = []
+        dp = [[float('inf')]*2 for _ in range(len(regular)+1)]
+        dp[0][0] = 0
+        dp[0][1] = expressCost
+        for i in range(1, len(regular)+1):
+            dp[i][0] = min(
+                dp[i-1][0] + regular[i-1],
+                dp[i-1][1] + express[i-1]
+            )
+            dp[i][1] = min(
+                dp[i-1][1] + express[i-1],
+                dp[i-1][0] + regular[i-1] + expressCost
+            )
+            res.append(min(dp[i]))
         return res
