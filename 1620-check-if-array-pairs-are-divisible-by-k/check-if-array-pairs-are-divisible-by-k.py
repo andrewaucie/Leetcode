e1c8b093c5f4ab603f4ad3dbe26b4417bc0a5095
andrewaucie <aucieandrew@gmail.com>
2024-10-01 03:13:37 -0400

    Time: 471 ms (87.56%) | Memory: 30.3 MB (91.94%) - LeetSync

--- 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.py
+++ 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.py
@@ -1,14 +1,16 @@
 class Solution:
     def canArrange(self, arr: List[int], k: int) -> bool:
-        remainderList = [n%k for n in arr]
-        remainders = Counter(remainderList)
-        for r in remainderList:
-            if k - r in remainders or (r == 0 and remainders[r] >= 2):
-                remainders[r] -= 1 + int(r == 0)
-                if remainders[k-r] > 0:
-                    remainders[k-r] -= 1
-                if remainders[r] == 0:
-                    del remainders[r]
-                if k-r in remainders and remainders[k-r] == 0:
-                    del remainders[k-r]
-        return len(remainders) == 0
+        freq = [0] * k
+        
+        for num in arr:
+            remainder = (num % k + k) % k
+            freq[remainder] += 1
+        
+        if freq[0] % 2 != 0:  
+            return False
+        
+        for i in range(1, k // 2 + 1):
+            if freq[i] != freq[k - i]:  
+                return False
+        
+        return True
