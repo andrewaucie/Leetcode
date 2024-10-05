4dd3a58ed52c34d4d4994e073688f737e138f47c
andrewaucie <aucieandrew@gmail.com>
2024-10-05 17:30:58 -0400

    Time: 77 ms (32.16%) | Memory: 20.8 MB (19.37%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.py
+++ 214-shortest-palindrome/shortest-palindrome.py
@@ -1,31 +1,18 @@
 class Solution:
     def shortestPalindrome(self, s: str) -> str:
-        # Reverse the original string
-        reversed_string = s[::-1]
-
-        # Combine the original and reversed strings with a separator
-        combined_string = s + "#" + reversed_string
-
-        # Build the prefix table for the combined string
-        prefix_table = self._build_prefix_table(combined_string)
-
-        # Get the length of the longest palindromic prefix
-        palindrome_length = prefix_table[-1]
-
-        # Construct the shortest palindrome by appending the reverse of the suffix
-        suffix = reversed_string[: len(s) - palindrome_length]
-        return suffix + s
-
-    # Helper function to build the KMP prefix table
-    def _build_prefix_table(self, s: str) -> list:
-        prefix_table = [0] * len(s)
+        reversedString = s[::-1]
+        string = s + "#" + reversedString
+        lps = self.LPS(string)[-1]
+        prepend = reversedString[: len(s) - lps]
+        return prepend + s
+    
+    def LPS(self, s):
         length = 0
-
-        # Build the table by comparing characters
+        table = [0] * len(s)
         for i in range(1, len(s)):
-            while length > 0 and s[i] != s[length]:
-                length = prefix_table[length - 1]
+            if length > 0 and s[i] != s[length]:
+                length = table[length - 1]
             if s[i] == s[length]:
                 length += 1
-            prefix_table[i] = length
-        return prefix_table
+            table[i] = length
+        return table
