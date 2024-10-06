2cb965f86916bd12d4cbc6bcfb6e4d2236f9b508
andrewaucie <aucieandrew@gmail.com>
2024-10-06 03:45:27 -0400

    Time: 94 ms (55.43%) | Memory: 24.4 MB (35.94%) - LeetSync

--- 923-super-egg-drop/super-egg-drop.py
+++ 923-super-egg-drop/super-egg-drop.py
@@ -1,9 +1,11 @@
 class Solution:
     def superEggDrop(self, k: int, n: int) -> int:
         dp = [[0]*(k+1) for _ in range(n+1)]
+        
+        attempts = 0
 
-        for i in range(1, n+1):
-            for j in range(1, k+1):
-                dp[i][j] = dp[i-1][j-1] + dp[i-1][j] + 1
-            if dp[i][k] >= n:
-                return i
+        while dp[attempts][k] < n:
+            attempts += 1
+            for eggs in range(1, k+1):
+                dp[attempts][eggs] = dp[attempts-1][eggs-1] + dp[attempts-1][eggs] + 1
+        return attempts
