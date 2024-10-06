2724daf10ed1b0724c32c8f17416eae4b141f0c1
andrewaucie <aucieandrew@gmail.com>
2024-10-06 03:40:24 -0400

    Time: 93 ms (56.39%) | Memory: 24.3 MB (54.31%) - LeetSync

---
+++ 923-super-egg-drop/super-egg-drop.py
@@ -0,0 +1,9 @@
+class Solution:
+    def superEggDrop(self, k: int, n: int) -> int:
+        dp = [[0]*(k+1) for _ in range(n+1)]
+
+        for i in range(1, n+1):
+            for j in range(1, k+1):
+                dp[i][j] = dp[i-1][j-1] + dp[i-1][j] + 1
+            if dp[i][k] >= n:
+                return i
