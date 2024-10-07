79c588501bd33bd9aba1d9eacbcd70dba3dd3bbe
andrewaucie <aucieandrew@gmail.com>
2024-10-07 02:44:52 -0400

    Time: 204 ms (35.03%) | Memory: 18.8 MB (69.35%) - LeetSync

--- 688-knight-probability-in-chessboard/knight-probability-in-chessboard.py
+++ 688-knight-probability-in-chessboard/knight-probability-in-chessboard.py
@@ -2,17 +2,16 @@ class Solution:
     def knightProbability(self, n: int, k: int, row: int, column: int) -> float:
         # recurse on moves, multiply by number of moves in bound / 8
         knightMoves = {(1,2),(2,1),(-1,2),(1,-2),(-1,-2),(2,-1),(-2,1),(-2,-1)}
-        memo = {}
-        def move(k, r, c):
-            if k == 0:
-                return 1
-            if (k,r,c) in memo:
-                return memo[(k,r,c)]
-            paths = 0
-            for dx,dy in knightMoves:
-                if 0 <= r+dx < n and 0 <= c+dy < n:
-                    paths += move(k-1, r+dx, c+dy)
-            memo[(k,r,c)] = paths
-            return paths
+
+        dp = [[[0]*n for _ in range(n)] for _ in range(k+1)]
+        dp[0][row][column] = 1
+
+        for move in range(1, k+1):
+            for i in range(n):
+                for j in range(n):
+                    for dx,dy in knightMoves:
+                        if 0 <= i+dx < n and 0 <= j+dy < n:
+                            dp[move][i][j] += dp[move-1][i+dx][j+dy]
         
-        return move(k, row, column) / 8**k
+        totalMoves = sum(dp[k][i][j] for i in range(n) for j in range(n))
+        return totalMoves / 8**k
