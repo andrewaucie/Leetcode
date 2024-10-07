e41c0ccc885ab2bcae7aecd4e042ec2bcf5f816e
andrewaucie <aucieandrew@gmail.com>
2024-10-06 22:02:41 -0400

    Time: 1589 ms (9.57%) | Memory: 35.8 MB (21.21%) - LeetSync

--- 923-super-egg-drop/super-egg-drop.py
+++ 923-super-egg-drop/super-egg-drop.py
@@ -1,11 +1,25 @@
 class Solution:
     def superEggDrop(self, k: int, n: int) -> int:
-        dp = [[0]*(k+1) for _ in range(n+1)]
-        
-        attempts = 0
-
-        while dp[attempts][k] < n:
-            attempts += 1
-            for eggs in range(1, k+1):
-                dp[attempts][eggs] = dp[attempts-1][eggs-1] + dp[attempts-1][eggs] + 1
-        return attempts
+        memo = {}
+        def memoization(k, n):
+            if n == 0 or n == 1 or k == 1:
+                return n
+            if (k,n) in memo:
+                return memo[(k,n)]
+            minAttempts = float('inf')
+            l,r = 1, n
+            while l <= r:
+                mid = (l+r) // 2
+                # break, go down
+                down = memoization(k-1, mid-1)
+                # no break, go up
+                up = memoization(k, n-mid)
+                temp = max(down,up) + 1
+                if down < up:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+                minAttempts = min(minAttempts, temp)
+            memo[(k,n)] = minAttempts
+            return minAttempts
+        return memoization(k,n)
