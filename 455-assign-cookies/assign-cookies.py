e35dd6c28f7c2c56d23ec5f9c53353cc3b4adef5
andrewaucie <aucieandrew@gmail.com>
2024-10-08 00:07:53 -0400

    Time: 299 ms (73.35%) | Memory: 19.1 MB (42.08%) - LeetSync

--- 455-assign-cookies/assign-cookies.py
+++ 455-assign-cookies/assign-cookies.py
@@ -5,8 +5,6 @@ class Solution:
         child, cookie = 0, 0
         while child < len(g) and cookie < len(s):
             if s[cookie] >= g[child]:
-                cookie += 1
                 child += 1
-            else:
-                cookie += 1
+            cookie += 1
         return child
