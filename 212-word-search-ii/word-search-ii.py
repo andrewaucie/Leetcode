092c298d406322c037840381d5200a9264da25ad
andrewaucie <aucieandrew@gmail.com>
2024-10-12 00:17:01 -0400

    Time: 7467 ms (14.23%) | Memory: 281.7 MB (5.78%) - LeetSync

--- 212-word-search-ii/word-search-ii.py
+++ 212-word-search-ii/word-search-ii.py
@@ -13,19 +13,18 @@ class Solution:
             curr = trie
         
         res = []
-        def dfs(i, j, string, curr):
-            visited.add((i,j))
+        def dfs(i, j, string, curr, visited):
             if '*' in curr:
                 res.append(string)
             for dx, dy in {(1,0), (0,1), (-1,0), (0,-1)}:
                 if 0 <= i+dx < n and 0 <= j+dy < m and (i+dx, j+dy) not in visited:
                     if board[i+dx][j+dy] in curr:
-                        dfs(i+dx, j+dy, string + board[i+dx][j+dy], curr[board[i+dx][j+dy]])
+                        visited.add((i+dx, j+dy))
+                        dfs(i+dx, j+dy, string + board[i+dx][j+dy], curr[board[i+dx][j+dy]], visited)
                         visited.remove((i+dx, j+dy))
-
         for i in range(n):
             for j in range(m):
                 if board[i][j] in trie:
-                    visited = set()
-                    dfs(i, j, board[i][j], trie[board[i][j]])
+                    visited = set([(i,j)])
+                    dfs(i, j, board[i][j], trie[board[i][j]], visited)
         return set(res)
