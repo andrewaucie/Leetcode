51a1aad0b0dc3426485b0d8511a02a95efebafe0
andrewaucie <aucieandrew@gmail.com>
2024-10-13 19:25:31 -0400

    Time: 6377 ms (100.00%) | Memory: 277.3 MB (100.00%) - LeetSync

---
+++ 3588-count-the-number-of-winning-sequences/count-the-number-of-winning-sequences.py
@@ -0,0 +1,29 @@
+class Solution:
+    def countWinningSequences(self, s: str) -> int:
+        combat = {
+            'E': 'F',
+            'W': 'E',
+            'F': 'W'
+        }
+        n = len(s)
+        memo = {}
+        def backtrack(i, points, prev):
+            if i == n:
+                return int(points > 0)
+            if points > 0 and points - (n - i + 1) > 0:
+                return 2**(n - i)
+            elif points < 0 and points + (n - i + 1) < 0:
+                return 0
+            if (i, points, prev) in memo:
+                return memo[(i, points, prev)]
+            ways = 0
+            for dragon in {'E', 'W', 'F'} - {prev}:
+                curr = points
+                if combat[s[i]] == dragon:
+                    curr += 1
+                elif combat[dragon] == s[i]:
+                    curr -= 1
+                ways += backtrack(i+1, curr, dragon)
+            memo[(i, points, prev)] = ways % (10**9 + 7)
+            return memo[(i, points, prev)]
+        return backtrack(0, 0, '')
