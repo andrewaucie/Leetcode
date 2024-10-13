9d18f9eb8e52252145f7af3721c48e04e5b6cdc0
andrewaucie <aucieandrew@gmail.com>
2024-10-13 02:52:46 -0400

    Time: 257 ms (12.03%) | Memory: 25.2 MB (5.75%) - LeetSync

--- 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.py
+++ 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.py
@@ -1,3 +1,5 @@
+from sortedcontainers import SortedList
+
 class Solution:
     def smallestRange(self, nums: List[List[int]]) -> List[int]:
         events = []
@@ -5,13 +7,18 @@ class Solution:
             for n in nums[i]:
                 events.append((n, i))
         events.sort()
-        ranges = {}
+        ranges = SortedList()
+        rangeDict = {}
         start = float('-inf')
         minRange = [float('-inf'), float('inf')]
         for n, rangeNum in events:
-            ranges[rangeNum] = n
-            start = min(ranges.values())
+            if rangeNum in rangeDict:
+                ranges.remove(rangeDict[rangeNum])
+            ranges.add(n)
+            rangeDict[rangeNum] = n
+            if len(ranges) > 1:
+                start = ranges[0]
             if len(ranges) == len(nums):
                 if n - start < minRange[1] - minRange[0]:
                     minRange = [start, n]
-        return minRange 
+        return minRange if minRange != [float('-inf'), float('inf')] else [nums[0][0], nums[0][0]]
