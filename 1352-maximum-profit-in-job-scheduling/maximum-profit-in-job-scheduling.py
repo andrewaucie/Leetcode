c2d533ce4a57a1c1e79b8973c56e02cad0c05b21
andrewaucie <aucieandrew@gmail.com>
2024-10-15 02:59:34 -0400

    Time: 605 ms (83.72%) | Memory: 32.1 MB (54.29%) - LeetSync

--- 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.py
+++ 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.py
@@ -1,31 +1,16 @@
-from typing import List
-import bisect
-
 class Solution:
-    def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
-        jobs = sorted(zip(startTime, endTime, profit), key=lambda x: x[0])
-        
-        memo = [-1] * len(jobs)
+  def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
+    maxProfit = 0
+    jobs = sorted([(s, e, p) for s, e, p in zip(startTime, endTime, profit)])
+    minHeap = []  
 
-        def findNextJob(index):
-            target_end = jobs[index][1]
-            low, high = index + 1, len(jobs)
-            while low < high:
-                mid = (low + high) // 2
-                if jobs[mid][0] >= target_end:
-                    high = mid
-                else:
-                    low = mid + 1
-            return low
+    
+    for i in range(len(startTime)):
+      startTime[i] = jobs[i][0]
 
-        def dp(i):
-            if i >= len(jobs):
-                return 0
-            if memo[i] != -1:
-                return memo[i]
-            skip = dp(i + 1)
-            next_job_index = findNextJob(i)
-            take = jobs[i][2] + dp(next_job_index)
-            memo[i] = max(skip, take)
-            return memo[i]
-        return dp(0)
+    for s, e, p in jobs:
+      while minHeap and s >= minHeap[0][0]:
+        maxProfit = max(maxProfit, heapq.heappop(minHeap)[1])
+      heapq.heappush(minHeap, (e, p + maxProfit))
+
+    return max(maxProfit, max(p for _, p in minHeap))
