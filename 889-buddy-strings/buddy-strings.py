80a753e20730c683a2bc22803a5df3dd88940535
andrewaucie <aucieandrew@gmail.com>
2024-10-15 12:48:47 -0400

    Time: 27 ms (96.93%) | Memory: 16.7 MB (63.85%) - LeetSync

--- 889-buddy-strings/buddy-strings.py
+++ 889-buddy-strings/buddy-strings.py
@@ -1,36 +1,27 @@
 class Solution:
     def buddyStrings(self, s: str, goal: str) -> bool:
-        if len(s) != len(goal):
+        ns = len(s)
+        ng = len(goal)
+
+        if ns != ng:
             return False
 
         if s == goal:
-            # If we have 2 same characters in string 's',
-            # we can swap them and still the strings will remain equal.
-            frequency = [0] * 26
+            farr = [0] * 26
             for ch in s:
-                frequency[ord(ch) - ord('a')] += 1
-                if frequency[ord(ch) - ord('a')] == 2:
+                farr[ord(ch) - ord('a')] += 1
+            
+            for count in farr:
+                if count > 1:
                     return True
-            # Otherwise, if we swap any two characters, it will make the strings unequal.
+            
             return False
 
-        firstIndex = -1
-        secondIndex = -1
-
-        for i in range(len(s)):
+        ans = []
+        for i in range(ns):
             if s[i] != goal[i]:
-                if firstIndex == -1:
-                    firstIndex = i
-                elif secondIndex == -1:
-                    secondIndex = i
-                else:
-                    # We have at least 3 indices with different characters,
-                    # thus, we can never make the strings equal with only one swap.
+                ans.append(i)
+                if len(ans) > 2:
                     return False
 
-        if secondIndex == -1:
-            # We can't swap if the character at only one index is different.
-            return False
-
-        # All characters of both strings are the same except two indices.
-        return s[firstIndex] == goal[secondIndex] and s[secondIndex] == goal[firstIndex]
+        return len(ans) == 2 and s[ans[0]] == goal[ans[1]] and s[ans[1]] == goal[ans[0]]
