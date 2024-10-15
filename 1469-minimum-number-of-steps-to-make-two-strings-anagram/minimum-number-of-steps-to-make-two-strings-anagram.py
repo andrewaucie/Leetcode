bdbae43933a2ff9543835779f0b289ba76ed4c3d
andrewaucie <aucieandrew@gmail.com>
2024-10-15 12:30:57 -0400

    Time: 86 ms (97.10%) | Memory: 17.3 MB (33.67%) - LeetSync

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.py
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -1,12 +1,26 @@
 class Solution:
     def minSteps(self, s: str, t: str) -> int:
-        freq = defaultdict(int)
-        steps = 0
-        for c in s:
-            freq[c] += 1
-        for c in t:
-            if freq[c] == 0:
-                steps += 1
-            else:
-                freq[c] -= 1
-        return steps
+        char_set = list(set(s))
+        over = 0
+        under = 0
+        miss_match = 0
+        seen_in_t = 0
+        for char in char_set:
+            s_count = s.count(char)
+            if char in t:
+                t_count = t.count(char)
+                seen_in_t += t_count
+                if t_count > s_count:
+                    over += t_count - s_count
+                elif t_count < s_count:
+                    under += s_count-t_count
+        miss_match += abs(seen_in_t-len(t))
+        moves= 0
+        if over > under:
+            moves += under
+            moves += miss_match
+            moves += (over-under)
+        else:
+            moves += over
+            moves += miss_match
+        return moves
