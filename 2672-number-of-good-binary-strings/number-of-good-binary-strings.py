c9c1b47aa2cab4049c4a26163926c136ae38f308
andrewaucie <aucieandrew@gmail.com>
2024-10-20 15:24:29 -0400

    Time: 739 ms (6.42%) | Memory: 679.8 MB (22.64%) - LeetSync

--- 2672-number-of-good-binary-strings/number-of-good-binary-strings.py
+++ 2672-number-of-good-binary-strings/number-of-good-binary-strings.py
@@ -1,15 +1,16 @@
 class Solution:
     def goodBinaryStrings(self, minLength: int, maxLength: int, oneGroup: int, zeroGroup: int) -> int:
-        
-        dp, g = [1]+[0] * maxLength, gcd(oneGroup,zeroGroup)
-
+        res = 0
+        g = gcd(oneGroup, zeroGroup)
+        dp = [0] * (maxLength + 1)
+        dp[0] = 1
         for i in range(1, maxLength + 1):
-
-            if i%g:continue             #<--- no solutions if i%g != 0
-
-            dp[i] += ((dp[i - oneGroup ] if i >= oneGroup  else 0)+
-                      (dp[i - zeroGroup] if i >= zeroGroup else 0))
-
-            dp[i]%= 1_000_000_007
-
-        return sum(dp[minLength : maxLength + 1]) % 1000000007
+            if i%g:
+                continue
+            if oneGroup <= i:
+                dp[i] += dp[i - oneGroup]
+            if zeroGroup <= i:
+                dp[i] += dp[i - zeroGroup]
+            if i >= minLength:
+                res += dp[i]
+        return res % (10**9 + 7)
