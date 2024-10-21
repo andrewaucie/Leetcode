f9585ae1d6616788cdb75b880e14ecb22ddb8676
andrewaucie <aucieandrew@gmail.com>
2024-10-21 16:11:09 -0400

    Time: 283 ms (40.72%) | Memory: 16.6 MB (55.73%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.py
+++ 37-sudoku-solver/sudoku-solver.py
@@ -1,39 +1,31 @@
 class Solution:
     def solveSudoku(self, board: List[List[str]]) -> None:
-        """
-        Do not return anything, modify board in-place instead.
-        """
-        
-        rows = defaultdict(set)
-        cols = defaultdict(set)
-        box = defaultdict(set)
-        for i in range(len(board)):
-            for j in range(len(board)):
-                if board[i][j] != '.':
-                    rows[i].add(board[i][j])
-                    cols[j].add(board[i][j])
-                    box[(i // 3, j // 3)].add(board[i][j])
-        def backtrack(i,j):
-            if j >= len(board):
-                i += 1
-                j = 0
-            if i >= len(board):
+        # Check if placing 'k' at position (r, c) is valid
+        def isValid(r, c, k):
+            for i in range(9):
+                if board[i][c] == k or board[r][i] == k:
+                    return False
+                subgrid_row = 3 * (r // 3) + i // 3
+                subgrid_col = 3 * (c // 3) + i % 3
+                if board[subgrid_row][subgrid_col] == k:
+                    return False
+            return True    
+
+        def fill(r, c):
+            if r == 9:
                 return True
-            if board[i][j] != ".":
-                return backtrack(i, j+1)
-            for n in range(1, 10):
-                n = str(n)
-                if (n not in rows[i]) and (n not in cols[j]) and (n not in box[(i // 3, j // 3)]):
-                    rows[i].add(n)
-                    cols[j].add(n)
-                    box[(i // 3, j // 3)].add(n)
-                    board[i][j] = n
-                    if backtrack(i, j+1):
-                        return True
-                    board[i][j] = "."
-                    rows[i].remove(n)
-                    cols[j].remove(n)
-                    box[(i // 3, j // 3)].remove(n)
-            return False
-        backtrack(0,0)
-        
+            
+            if c == 9:
+                return fill(r + 1, 0)
+
+            if board[r][c] == '.':
+                for k in range(1, 10):
+                    if isValid(r, c, str(k)):
+                        board[r][c] = str(k)
+                        if fill(r, c + 1):
+                            return True
+                        board[r][c] = '.'
+                return False
+            return fill(r, c + 1)
+
+        fill(0, 0)
