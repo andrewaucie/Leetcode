af5c22213ed9b8038ea5c044fc9fb50205fa5d41
andrewaucie <aucieandrew@gmail.com>
2024-10-22 12:57:24 -0400

    Time: 1591 ms (8.78%) | Memory: 35.8 MB (20.53%) - LeetSync

--- 923-super-egg-drop/super-egg-drop.py
+++ 923-super-egg-drop/super-egg-drop.py
@@ -1,33 +1,25 @@
 class Solution:
     def superEggDrop(self, k: int, n: int) -> int:
-        def ed(e,f,dp):
-            if f == 0 or f == 1:
-                return f
-            if e == 1:
-                return f
-            if dp[e][f] != -1:
-                return dp[e][f]
-            mini = 10001
-            l = 1
-            h = f
-            while l<=h:
-                k = (l+h)//2
-                if dp[e-1][k-1] != -1:
-                    low = dp[e-1][k-1]
+        memo = {}
+        def memoization(k, n):
+            if n == 0 or n == 1 or k == 1:
+                return n
+            if (k,n) in memo:
+                return memo[(k,n)]
+            minAttempts = float('inf')
+            l,r = 1, n
+            while l <= r:
+                mid = (l+r) // 2
+                # break, go down
+                down = memoization(k-1, mid-1)
+                # no break, go up
+                up = memoization(k, n-mid)
+                temp = max(down,up) + 1
+                if down < up:
+                    l = mid + 1
                 else:
-                    low = ed(e-1,k-1,dp)
-                if dp[e][f-k] != -1:
-                    high = dp[e][f-k]
-                else:
-                    high = ed(e,f-k,dp)
-                temp = max(high,low) + 1
-                if high > low:
-                    l = k+1
-                else:
-                    h = k-1
-                mini = min(mini,temp)
-            dp[e][f] = mini
-            return mini
-        dp = [[-1 for i in range(n+1)] for i in range(k+1)]
-        ans = ed(k,n,dp)
-        return ans
+                    r = mid - 1
+                minAttempts = min(minAttempts, temp)
+            memo[(k,n)] = minAttempts
+            return minAttempts
+        return memoization(k,n)
