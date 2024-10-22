a85c56d2b288124b57f285601eebed4a31bdf57d
andrewaucie <aucieandrew@gmail.com>
2024-10-22 03:01:00 -0400

    Time: 250 ms (100.00%) | Memory: 33.3 MB (5.06%) - LeetSync

--- 3196-apply-operations-to-maximize-frequency-score/apply-operations-to-maximize-frequency-score.py
+++ 3196-apply-operations-to-maximize-frequency-score/apply-operations-to-maximize-frequency-score.py
@@ -5,10 +5,12 @@ class Solution:
         for n in nums:
             prefix.append(n + prefix[-1])
         left = 0
+        maxLen = 0
         for right in range(1, len(prefix)):
             mid = (left + right) // 2
             val = (prefix[right] - prefix[mid]) - (right - mid) * nums[mid]
             val += (mid - left) * nums[mid] - (prefix[mid] - prefix[left])
             if val > k:
                 left += 1
-        return right - left
+            maxLen = max(maxLen, right - left)
+        return maxLen
