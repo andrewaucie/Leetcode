12743a11abf161e4f3ae3a36d6a4f69faf3a52bf
andrewaucie <aucieandrew@gmail.com>
2024-10-22 02:03:29 -0400

    Time: 211 ms (100.00%) | Memory: 33.3 MB (5.06%) - LeetSync

---
+++ 3196-apply-operations-to-maximize-frequency-score/apply-operations-to-maximize-frequency-score.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxFrequencyScore(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        prefix = [0]
+        for n in nums:
+            prefix.append(n + prefix[-1])
+        left = 0
+        for right in range(1, len(prefix)):
+            mid = (left + right) // 2
+            val = (prefix[right] - prefix[mid]) - (right - mid) * nums[mid]
+            val += (mid - left) * nums[mid] - (prefix[mid] - prefix[left])
+            if val > k:
+                left += 1
+        return right - left
