c658da5712183d50d51d19b9a852afbaaaf24e7e
andrewaucie <aucieandrew@gmail.com>
2024-10-26 02:17:09 -0400

    Time: 1193 ms (31.15%) | Memory: 46.2 MB (5.83%) - LeetSync

--- 976-minimum-area-rectangle/minimum-area-rectangle.py
+++ 976-minimum-area-rectangle/minimum-area-rectangle.py
@@ -10,13 +10,16 @@ class Solution:
                 for i in range(len(pointMap[y])):
                     for j in range(i+1, len(pointMap[y])):
                         key = sorted([pointMap[y][i], pointMap[y][j]])
-                        pairXMap[tuple(key)].append(y)
+                        heapq.heappush(pairXMap[tuple(key)], y)
         minArea = float('inf')
         for (x1, x2) in pairXMap:
-            yPoints = sorted(pairXMap[(x1,x2)])
+            yPoints = pairXMap[(x1,x2)]
             if len(yPoints) > 1:
                 minDist = float('inf')
-                for i in range(1, len(yPoints)):
-                    minDist = min(minDist, yPoints[i] - yPoints[i-1])
+                prev = heapq.heappop(yPoints)
+                while yPoints:
+                    curr = heapq.heappop(yPoints)
+                    minDist = min(minDist, curr - prev)
+                    prev = curr
                 minArea = min(minArea, abs((x2-x1)*minDist))
         return minArea if minArea != float('inf') else 0
