b407d25f17c364a7c06fb9c0adc77cc49748f3a6
andrewaucie <aucieandrew@gmail.com>
2024-10-26 02:24:23 -0400

    Time: 1004 ms (49.71%) | Memory: 16.6 MB (99.72%) - LeetSync

--- 976-minimum-area-rectangle/minimum-area-rectangle.py
+++ 976-minimum-area-rectangle/minimum-area-rectangle.py
@@ -1,13 +1,10 @@
 class Solution:
     def minAreaRect(self, points: List[List[int]]) -> int:
-        pointMap = defaultdict(set)
-        for x,y in points:
-            pointMap[x].add(y)
-
-        minArea = float('inf')
-        for x1,y1 in points:
-            for x2,y2 in points:
-                if x1 != x2 and y1 != y2:
-                    if y2 in pointMap[x1] and y1 in pointMap[x2]:
-                        minArea = min(minArea, abs(x1-x2) * abs(y1-y2))
-        return minArea if minArea != float('inf') else 0
+        seen = set()
+        ans = sys.maxsize
+        for x1, y1 in points:
+            for x2, y2 in points:
+                if (x1,y2) in seen and (x2,y1) in seen:
+                    ans = min(ans, abs((x2-x1)*(y2-y1)))
+            seen.add((x1,y1))
+        return ans if ans != sys.maxsize else 0
