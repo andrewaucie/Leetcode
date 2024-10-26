880e75e2a953cfccdc45b734521af54f92d8e953
andrewaucie <aucieandrew@gmail.com>
2024-10-26 02:21:29 -0400

    Time: 1383 ms (24.26%) | Memory: 16.9 MB (48.72%) - LeetSync

--- 976-minimum-area-rectangle/minimum-area-rectangle.py
+++ 976-minimum-area-rectangle/minimum-area-rectangle.py
@@ -1,25 +1,13 @@
 class Solution:
     def minAreaRect(self, points: List[List[int]]) -> int:
-        pointMap = defaultdict(list)
+        pointMap = defaultdict(set)
         for x,y in points:
-            pointMap[y].append(x)
-        
-        pairXMap = defaultdict(list)
-        for y in pointMap:
-            if len(pointMap[y]) > 1:
-                for i in range(len(pointMap[y])):
-                    for j in range(i+1, len(pointMap[y])):
-                        key = sorted([pointMap[y][i], pointMap[y][j]])
-                        heapq.heappush(pairXMap[tuple(key)], y)
+            pointMap[x].add(y)
+
         minArea = float('inf')
-        for (x1, x2) in pairXMap:
-            yPoints = pairXMap[(x1,x2)]
-            if len(yPoints) > 1:
-                minDist = float('inf')
-                prev = heapq.heappop(yPoints)
-                while yPoints:
-                    curr = heapq.heappop(yPoints)
-                    minDist = min(minDist, curr - prev)
-                    prev = curr
-                minArea = min(minArea, abs((x2-x1)*minDist))
+        for x1,y1 in points:
+            for x2,y2 in points:
+                if x1 != x2 and y1 != y2:
+                    if y2 in pointMap[x1] and y1 in pointMap[x2]:
+                        minArea = min(minArea, abs(x1-x2) * abs(y1-y2))
         return minArea if minArea != float('inf') else 0
