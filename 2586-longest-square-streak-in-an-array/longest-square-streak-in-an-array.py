784cac55e90edfe6022c918a79aa94816b17df2c
andrewaucie <aucieandrew@gmail.com>
2024-10-28 01:17:42 -0400

    Time: 152 ms (32.43%) | Memory: 37 MB (17.00%) - LeetSync

--- 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.py
+++ 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.py
@@ -1,36 +1,23 @@
 class Solution:
     def longestSquareStreak(self, nums: List[int]) -> int:
-        longest = 1
-        numMap = {}
-        for n in set(nums):
-            numMap[n] = [n,n,1]
-            if n**2 in numMap:
-                # n marks start of this streak
-                numMap[n][1] = numMap[n**2][1]  # update end on curr num
-                numMap[n][2] += numMap[n**2][2] # update size
-                numMap[numMap[n**2][0]] = numMap[n]
-                numMap[numMap[n**2][1]] = numMap[n]
+        # Dictionary to store the length of square streak for each number
+        streak_lengths = {}
 
-            if n**0.5 in numMap:
-                # n marks end of this streak
-                numMap[n][0] = numMap[n**0.5][0]  # update start on curr num
-                numMap[n][2] += numMap[n**0.5][2] # update size
-                numMap[numMap[n**0.5][0]] = numMap[n]
-                numMap[numMap[n**0.5][1]] = numMap[n]
+        nums.sort()
 
-            longest = max(longest, numMap[n][2])
-        return longest if longest != 1 else -1
+        for number in nums:
+            root = int(number**0.5)
 
-        # [2,4,16,256,...]
-        # [3,9,81,6561,..]
-        # [5,25,625,..]
-        # [6,36,..]
-        # [8,64,..]
-        # input = [4,3,6,16,8,2]
+            # Check if the number is a perfect square and its square root is in the dictionary
+            if root * root == number and root in streak_lengths:
+                # Extend the streak from its square root
+                streak_lengths[number] = streak_lengths[root] + 1
+            else:
+                # Start a new streak
+                streak_lengths[number] = 1
 
-        # [3,9,81,6561]
-        # {n: [start, end, size]}
-        # {4,3,6,16}
-        # {3: [3,3,1], 6: [6,6,1], 16: [2,16,3], 2:[2,16,3], 4:[2,16,3], 256}
+        # Find the maximum streak length
+        longest_streak = max(streak_lengths.values(), default=0)
 
-        # union find approach to connect streaks, get max size streak at the end (or maintain max size throughout)
+        # Return -1 if no valid streak found, otherwise return the longest streak
+        return longest_streak if longest_streak > 1 else -1
