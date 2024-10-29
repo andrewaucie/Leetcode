e3cbcdff0f655496b2dc3243b0f0d5a6cdcb4e0d
andrewaucie <aucieandrew@gmail.com>
2024-10-29 16:46:05 -0400

    Time: 183 ms (46.38%) | Memory: 41.9 MB (5.71%) - LeetSync

--- 2794-maximum-number-of-moves-in-a-grid/maximum-number-of-moves-in-a-grid.py
+++ 2794-maximum-number-of-moves-in-a-grid/maximum-number-of-moves-in-a-grid.py
@@ -1,13 +1,15 @@
 class Solution:
     def maxMoves(self, grid: List[List[int]]) -> int:
-        #cache = defaultdict(int)
-        @cache
+        cache = defaultdict(int)
         def dfs(i,j):
+            if (i,j) in cache:
+                return cache[(i,j)]
             moves = 0
             for dx in {-1, 0, 1}:
                 if 0 <= i+dx < len(grid) and 0 <= j+1 < len(grid[0]):
                     if grid[i+dx][j+1] > grid[i][j]:
                         moves = max(moves, dfs(i+dx, j+1) + 1)
+            cache[(i,j)] = moves
             return moves
         
         maxMoves = 0
