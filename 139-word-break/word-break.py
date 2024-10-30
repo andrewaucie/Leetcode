a6c7d64e4bf2014b5c95b1a8ee230fc84704b414
andrewaucie <aucieandrew@gmail.com>
2024-10-29 22:43:12 -0400

    Time: 3 ms (69.75%) | Memory: 17 MB (10.91%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -2,14 +2,16 @@ class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         wordSet = set(wordDict)
         cache = {}
-        def dfs(i, curr):
+        def dfs(i):
             if i == len(s):
-                return curr in wordSet
-            if (i, curr) in cache:
-                return cache[(i, curr)]
-            skipWord = dfs(i+1, curr + s[i])
-            takeWord = (curr in wordSet) and dfs(i+1, s[i])
-            cache[(i,curr)] = skipWord or takeWord
-            return cache[(i,curr)]
-        return dfs(0,"")
+                return True
+            if i in cache:
+                return cache[i]
+            takeWord = False
+            for j in range(i, len(s)):
+                if s[i:j+1] in wordSet:
+                    takeWord |= dfs(j+1)
+            cache[i] = takeWord
+            return cache[i]
+        return dfs(0)
 
