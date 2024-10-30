3f3663078fa2001190c0f64ca3bf9d3a34c7260d
andrewaucie <aucieandrew@gmail.com>
2024-10-30 10:33:07 -0400

    Time: 38 ms (39.15%) | Memory: 18.5 MB (30.19%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.py
+++ 876-hand-of-straights/hand-of-straights.py
@@ -1,16 +1,21 @@
 class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
-        n = len(hand)
-        if n % groupSize != 0:
-            return False
+        # [1,2,3,6,2,3,4,7,8]
+        freq = Counter(hand)
+        sortedHands = []
+        for key in sorted(freq.keys()):
+            sortedHands.append([key, freq[key]])
         
-        count = Counter(hand)
-        sorted_keys = sorted(count.keys())
-        for key in sorted_keys:
-            if count[key] > 0:
-                start_count = count[key]
-                for i in range(key, key + groupSize):
-                    if count[i] < start_count:
-                        return False
-                    count[i] -= start_count
-        return True
+        for i in range(len(sortedHands) - groupSize + 1):
+            if sortedHands[i][1] == 0:
+                continue
+            for j in range(i+1, i+groupSize):
+                if (sortedHands[j-1][0] + 1 == sortedHands[j][0] and sortedHands[j][1] >= sortedHands[i][1]):
+                    sortedHands[j][1] -= sortedHands[i][1]
+                else:
+                    return False
+            sortedHands[i][1] = 0
+        return all(sortedHands[i][1] == 0 for i in range(len(sortedHands)))
+
+        # [(1: 1), (2: 2), (3: 2), (4: 1), (6: 1), (7: 1), (8: 1)]
+
