43027da37d34aade84ea5f56cabb9887268f44f2
andrewaucie <aucieandrew@gmail.com>
2024-10-30 10:37:46 -0400

    Time: 36 ms (44.92%) | Memory: 18 MB (90.68%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.py
+++ 876-hand-of-straights/hand-of-straights.py
@@ -1,21 +1,20 @@
 class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
         # [1,2,3,6,2,3,4,7,8]
+        n = len(hand)
+        if n % groupSize != 0:
+            return False
+
         freq = Counter(hand)
-        sortedHands = []
         for key in sorted(freq.keys()):
-            sortedHands.append([key, freq[key]])
-        
-        for i in range(len(sortedHands) - groupSize + 1):
-            if sortedHands[i][1] == 0:
-                continue
-            for j in range(i+1, i+groupSize):
-                if (sortedHands[j-1][0] + 1 == sortedHands[j][0] and sortedHands[j][1] >= sortedHands[i][1]):
-                    sortedHands[j][1] -= sortedHands[i][1]
-                else:
-                    return False
-            sortedHands[i][1] = 0
-        return all(sortedHands[i][1] == 0 for i in range(len(sortedHands)))
+            if freq[key] > 0:
+                count = freq[key]
+                for nextKey in range(key, key + groupSize):
+                    if nextKey not in freq or freq[nextKey] < count:
+                        return False
+                    freq[nextKey] -= count
+        return True
+
 
         # [(1: 1), (2: 2), (3: 2), (4: 1), (6: 1), (7: 1), (8: 1)]
 
