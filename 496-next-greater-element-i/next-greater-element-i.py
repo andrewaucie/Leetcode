36a015cd6c88e6f8929af0b3107e76569c960f05
andrewaucie <aucieandrew@gmail.com>
2024-10-31 01:51:23 -0400

    Time: 0 ms (100.00%) | Memory: 16.8 MB (42.10%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,13 +1,22 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        indexMap = {nums2[i] : i for i in range(len(nums2))}
-        res = []
-        for n in nums1:
-            greater = -1
-            for i in range(indexMap[n]+1, len(nums2)):
-                if nums2[i] > n:
-                    greater = nums2[i]
-                    break
-            res.append(greater)
+        # [4,1,2]
+        # [1,3,4,2]
+
+        # [1,3]
+        # [3]
+        # monotonic stack of nums2
+        # for n in nums2:
+        #     while n > stack[-1]:
+        #          
+        #     stack.append(n)
+        indexMap = {nums1[i] : i for i in range(len(nums1))}
+        res = [-1] * len(nums1)
+        stack = []
+        for n in nums2:
+            while stack and n > stack[-1]:
+                poppedNum = stack.pop()
+                if poppedNum in indexMap:
+                    res[indexMap[poppedNum]] = n
+            stack.append(n)
         return res
-            
