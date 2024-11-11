93095117f7915c84e0d2024f6ed3f9284d88f244
andrewaucie <aucieandrew@gmail.com>
2024-11-11 00:27:07 -0500

    Time: 1 ms (67.81%) | Memory: 16.7 MB (11.18%) - LeetSync

--- 778-reorganize-string/reorganize-string.py
+++ 778-reorganize-string/reorganize-string.py
@@ -6,16 +6,19 @@ class Solution:
         if 2 * maxCount - 1 > len(s):
             return ""
         
-        max_heap = [(-count, char) for char, count in freq.items()]
-        heapq.heapify(max_heap)
+        res = [''] * len(s)
+        index = 0
+
+        while freq[maxChar] > 0:
+            res[index] = maxChar
+            index += 2
+            freq[maxChar] -= 1
         
-        string = ""
-        prev_count, prev_char = 0, ''
-        
-        while max_heap:
-            count, char = heapq.heappop(max_heap)
-            string += char
-            if prev_count < 0:
-                heapq.heappush(max_heap, (prev_count, prev_char))
-            prev_count, prev_char = count + 1, char
-        return string
+        for char, count in freq.items():
+            while count > 0:
+                if index >= len(s):
+                    index = 1
+                res[index] = char
+                index += 2
+                count -= 1
+        return ''.join(res)
