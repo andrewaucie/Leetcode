2b814ecf4e121da9956d24d8441930fd313edc69
andrewaucie <aucieandrew@gmail.com>
2024-11-25 18:52:47 -0500

    Time: 131 ms (86.83%) | Memory: 37.6 MB (60.91%) - LeetSync

--- 883-car-fleet/car-fleet.py
+++ 883-car-fleet/car-fleet.py
@@ -1,11 +1,15 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        cars = [(p,s) for p, s in zip(position, speed)]
-        cars.sort(key=lambda x:x[0], reverse=True)
-        fleets = []
-        for p,s in cars:
-            timeToTarget = (target - p) / s
-            fleets.append(timeToTarget)
-            if len(fleets) >= 2 and fleets[-1] <= fleets[-2]:
-                fleets.pop()
-        return len(fleets)
+        # [10,8,0,5,3]
+        # [2,4,1,1,3]
+        sortedCars = sorted(list(zip(position, speed)), reverse=True)
+        fleets = 0
+        # time to target = math.ceil((target - position) / speed)
+        # if time > prevTime, add to fleet
+        prevTime = -1
+        for pos, sp in sortedCars:
+            time = (target - pos) / sp
+            if time > prevTime:
+                fleets += 1
+                prevTime = time
+        return fleets
