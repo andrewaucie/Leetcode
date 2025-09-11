9801e4cc1107b3ba67e69a71b104c2a55a48d393
andrewaucie <aucieandrew@gmail.com>
2025-09-11 17:58:23 -0400

    Time: 219 ms (5.03%) | Memory: 48.2 MB (5.03%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -1,13 +1,32 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        nums = set(nums)
-        table = {}
-        maxval = 0
-        for num in nums:
-            x = table.get(num - 1, 0)
-            y = table.get(num + 1, 0)
-            val = x + y + 1
-            table[num - x] = val
-            table[num + y] = val
-            maxval = max(maxval, val)
-        return maxval
+        longest = 0
+        sequences = {}
+        for n in sorted(list(set(nums))):
+            # { n : [left, right, max] }
+            sequences[n] = [n, n, 1]
+            leftmost, rightmost = n, n
+            if n+1 in sequences:
+                rightmost = sequences[n+1][1]
+                # set n[right] to rightmost
+                sequences[n][1] = rightmost
+                # set rightmost[left] to n
+                sequences[rightmost][0] = n
+                # rightmost length + 1
+                sequences[n][2] += sequences[rightmost][2]
+
+            if n-1 in sequences:
+                leftmost = sequences[n-1][0]
+                # set n[left] to leftmost
+                sequences[n][0] = leftmost
+                # set leftmost[right] to right[n]
+                sequences[leftmost][1] = sequences[n][1]
+                # leftmost length + 1
+                sequences[n][2] += sequences[leftmost][2]
+            # update max
+            sequences[leftmost][2] = sequences[n][2]
+            sequences[rightmost][2] = sequences[n][2]
+
+            longest = max(longest, sequences[n][2])
+        return longest
+            
