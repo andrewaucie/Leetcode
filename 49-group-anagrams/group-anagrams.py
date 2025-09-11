d12dc9a34de93164feb5d4c877189446859ef3f7
andrewaucie <aucieandrew@gmail.com>
2025-09-11 00:20:05 -0400

    Time: 39 ms (7.74%) | Memory: 25.3 MB (11.87%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -3,14 +3,14 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         res = defaultdict(list)
         for word in strs:
-            # get word frequency
-            freq = defaultdict(int)
-            for w in word:
-                freq[w] += 1
-            # map freq to key
-            # tuple b/c list is unhashable
-            # sorted b/c list could be different order, maintains O(1) b/c |freq| <= 26
-            key = tuple(sorted(freq.items()))
-            res[key].append(word)
-            #res[(sorted(Counter(word).items()))].append(word)
+            # # get word frequency
+            # freq = defaultdict(int)
+            # for w in word:
+            #     freq[w] += 1
+            # # map freq to key
+            # # tuple b/c list is unhashable
+            # # sorted b/c list could be different order, maintains O(1) b/c |freq| <= 26
+            # key = tuple(sorted(freq.items()))
+            # res[key].append(word)
+            res[tuple(sorted(Counter(word).items()))].append(word)
         return [res[counter] for counter in res]
