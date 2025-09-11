d2eded9e8c28fa136aa17ab60c0011f9a6b26c8b
andrewaucie <aucieandrew@gmail.com>
2025-09-11 01:38:03 -0400

    Time: 25 ms (5.28%) | Memory: 32.3 MB (7.65%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,17 +1,27 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        freq = collections.defaultdict(int)
-        for n in nums:
-            freq[n] += 1
-        heap = []
-        for num, numFreq in freq.items():
-            if len(heap) == k and heap[0][0] < numFreq:
-                heapq.heappop(heap)
-            if len(heap) < k:
-                heapq.heappush(heap, (numFreq, num))
-        return [num for numFreq, num in heap]
-        # maxNum = max(nums)
-        # minNum = min(nums)
-        # buckets = [0] * (maxNum - minNum + 1)
+        # freq = collections.defaultdict(int)
         # for n in nums:
-        #     buckets[]
+        #     freq[n] += 1
+        # heap = []
+        # for num, numFreq in freq.items():
+        #     if len(heap) == k and heap[0][0] < numFreq:
+        #         heapq.heappop(heap)
+        #     if len(heap) < k:
+        #         heapq.heappush(heap, (numFreq, num))
+        # return [num for numFreq, num in heap]
+        freq = collections.defaultdict(int)
+        buckets = [set() for _ in range(len(nums)+1)]
+        for n in nums:
+            if n in freq:
+                buckets[freq[n]].remove(n)
+            freq[n] += 1
+            buckets[freq[n]].add(n)
+        topK = []
+        for i in range(len(buckets)-1, -1, -1):
+            for n in buckets[i]:
+                topK.append(n)
+                if len(topK) == k:
+                    return topK
+        return topK
+            
