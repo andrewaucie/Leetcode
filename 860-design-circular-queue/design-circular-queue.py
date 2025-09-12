782950a119073616d62742c8dbcb533a7cefaf37
andrewaucie <aucieandrew@gmail.com>
2025-09-12 19:54:40 -0400

    Time: 7 ms (59.62%) | Memory: 18.2 MB (99.21%) - LeetSync

---
+++ 860-design-circular-queue/design-circular-queue.py
@@ -0,0 +1,74 @@
+class Node:
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+    
+class MyCircularQueue:
+
+    def __init__(self, k: int):
+        self.queue = None
+        self.front = None
+        self.rear = None
+        self.k = k
+        self.size = 0
+
+    # [1] -> [2] -> [3]
+    # [2] -> [3]
+    # [2] -> [3] -> [4]
+    # { head : val, index : val, rear : val }
+
+    def enQueue(self, value: int) -> bool:
+        if self.isFull():
+            return False
+        self.size += 1
+        node = Node(val=value)
+        if not self.front:
+            self.front = node
+            self.rear = node
+        else:
+            self.rear.next = node
+            self.rear = node
+        return True
+
+    # [1]
+
+    # [1] -> [2]    
+    def deQueue(self) -> bool:
+        if self.isEmpty():
+            return False
+        self.size -= 1
+        # front == rear
+        if self.front == self.rear:
+            self.front = None
+            self.rear = None
+            return True
+        nextFront = self.front.next
+        self.front.next = None
+        self.front = nextFront
+        return True  
+
+    def Front(self) -> int:
+        if self.isEmpty():
+            return -1
+        return self.front.val
+
+    def Rear(self) -> int:
+        if self.isEmpty():
+            return -1
+        return self.rear.val
+
+    def isEmpty(self) -> bool:
+        return self.size == 0
+
+    def isFull(self) -> bool:
+        return self.size == self.k
+
+
+# Your MyCircularQueue object will be instantiated and called as such:
+# obj = MyCircularQueue(k)
+# param_1 = obj.enQueue(value)
+# param_2 = obj.deQueue()
+# param_3 = obj.Front()
+# param_4 = obj.Rear()
+# param_5 = obj.isEmpty()
+# param_6 = obj.isFull()
