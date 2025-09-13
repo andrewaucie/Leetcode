fc5599b7caa3ba74be5204cae525b7345f5fcb09
andrewaucie <aucieandrew@gmail.com>
2025-09-13 14:56:30 -0400

    Time: 64 ms (8.91%) | Memory: 20.4 MB (60.27%) - LeetSync

--- 341-flatten-nested-list-iterator/flatten-nested-list-iterator.py
+++ 341-flatten-nested-list-iterator/flatten-nested-list-iterator.py
@@ -1,46 +1,41 @@
-# """
-# This is the interface that allows for creating nested lists.
-# You should not implement it, or speculate about its implementation
-# """
-#class NestedInteger:
-#    def isInteger(self) -> bool:
-#        """
-#        @return True if this NestedInteger holds a single integer, rather than a nested list.
-#        """
-#
-#    def getInteger(self) -> int:
-#        """
-#        @return the single integer that this NestedInteger holds, if it holds a single integer
-#        Return None if this NestedInteger holds a nested list
-#        """
-#
-#    def getList(self) -> [NestedInteger]:
-#        """
-#        @return the nested list that this NestedInteger holds, if it holds a nested list
-#        Return None if this NestedInteger holds a single integer
-#        """
-
 class NestedIterator:
-    def __init__(self, nestedList: [NestedInteger]):
-        self.index = 0
-        self.list = [] 
-        self.flatten(nestedList)
     
-    def flatten(self, nestedList):
-        for it in nestedList:
-            if it.isInteger():
-                self.list.append(it.getInteger())
-            else:
-                self.flatten(it.getList())
+    def __init__(self, nestedList: [NestedInteger]):
+        self.stack = [[nestedList, 0]]
         
+    def make_stack_top_an_integer(self):
+        
+        while self.stack:
+            
+            # Essential for readability :)
+            current_list = self.stack[-1][0]
+            current_index = self.stack[-1][1]
+            
+            # If the top list is used up, pop it and its index.
+            if len(current_list) == current_index:
+                self.stack.pop()
+                continue
+            
+            # Otherwise, if it's already an integer, we don't need 
+            # to do anything.
+            if current_list[current_index].isInteger():
+                break
+            
+            # Otherwise, it must be a list. We need to increment the index
+            # on the previous list, and add the new list.
+            new_list = current_list[current_index].getList()
+            self.stack[-1][1] += 1 # Increment old.
+            self.stack.append([new_list, 0])
+            
+    
     def next(self) -> int:
-        val = self.list[self.index]
-        self.index += 1
-        return val
+        self.make_stack_top_an_integer()
+        current_list = self.stack[-1][0]
+        current_index = self.stack[-1][1]
+        self.stack[-1][1] += 1
+        return current_list[current_index].getInteger()
+        
     
     def hasNext(self) -> bool:
-        return self.index < len(self.list)
-
-# Your NestedIterator object will be instantiated and called as such:
-# i, v = NestedIterator(nestedList), []
-# while i.hasNext(): v.append(i.next())
+        self.make_stack_top_an_integer()
+        return len(self.stack) > 0
