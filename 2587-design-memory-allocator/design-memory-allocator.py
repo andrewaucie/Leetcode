bb5faa9d00cee4115dbdac5f90f541b86d73e5d5
andrewaucie <aucieandrew@gmail.com>
2025-09-13 22:38:44 -0400

    Time: 10 ms (94.15%) | Memory: 18.6 MB (73.93%) - LeetSync

--- 2587-design-memory-allocator/design-memory-allocator.py
+++ 2587-design-memory-allocator/design-memory-allocator.py
@@ -1,29 +1,65 @@
 class Allocator:
 
+    class Node:
+        def __init__(self, index: int, size: int, id: int):
+            self.index = index
+            self.size = size
+            self.id = id # -1 = free
+            self.prev = None
+            self.next = None
+
     def __init__(self, n: int):
-        self.n = n
-        self.memory = [0] * self.n
+        self.head = self.Node(0, n, -1)
+        self.used = defaultdict(list) # id : [Node]
 
     def allocate(self, size: int, mID: int) -> int:
-        index = 0
-        while index < self.n:
-            it = index
-            while it < self.n and self.memory[it] == 0:
-                if it == index + size - 1:
-                    for i in range(index, it+1):
-                        self.memory[i] = mID
-                    return index
-                it += 1
-            index = it + 1
+        node = self.head
+        while node:
+            if node.id == -1:
+                if node.size >= size:
+                    node.id = mID
+                    if node.size > size: 
+                        nextNode = self.Node(node.index + size, node.size - size, -1)
+                        node.size = size
+                        nextNext = node.next
+                        node.next = nextNode
+                        nextNode.prev = node
+                        nextNode.next = nextNext
+                        if nextNext:
+                            nextNext.prev = nextNode
+                    self.used[mID].append(node)
+                    return node.index
+            node = node.next
         return -1
-    
+
     def freeMemory(self, mID: int) -> int:
-        count = 0
-        for i in range(self.n):
-            if self.memory[i] == mID:
-                self.memory[i] = 0
-                count += 1
-        return count
+        if mID not in self.used:
+            return 0
+        ret = 0
+        for node in self.used[mID]:
+            if node.id != mID:
+                continue
+            ret += node.size
+            prevN = node.prev
+            nextN = node.next
+            node.id = -1
+            if nextN and nextN.id == -1:
+                # combine with next node
+                node.size += nextN.size
+                node.next = nextN.next
+                if nextN.next:
+                    nextN.next.prev = node
+            if prevN and prevN.id == -1:
+                # combine with prev node
+                prevN.size += node.size
+                prevN.next = node.next
+                if node.next:
+                    node.next.prev = prevN
+            
+        del self.used[mID]
+        return ret
+        
+        
 
 
 # Your Allocator object will be instantiated and called as such:
