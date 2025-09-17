970981cd4e9e117cb4fb0041d4e26c1f6cead04c
andrewaucie <aucieandrew@gmail.com>
2025-09-17 19:50:14 -0400

    Time: 31 ms (43.61%) | Memory: 18.1 MB (19.19%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -5,12 +5,11 @@ class Solution:
         left = 0
         for right in range(len(s2)):
             freq2[s2[right]] += 1
-            while left <= right and freq2[s2[right]] > freq1[s2[right]]:
+            while left < right and freq2[s2[left]] > freq1[s2[left]]:
                 freq2[s2[left]] -= 1
                 if freq2[s2[left]] == 0:
                     del freq2[s2[left]]
                 left += 1
             if freq1 == freq2:
                 return True
-            right += 1
         return False
