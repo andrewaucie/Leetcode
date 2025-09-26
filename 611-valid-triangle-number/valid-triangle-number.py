88f859015f275f8e26fab894d641f8030e0ba23d
andrewaucie <aucieandrew@gmail.com>
2025-09-26 14:38:49 -0400

    Time: 432 ms (41.77%) | Memory: 17.8 MB (82.72%) - LeetSync

--- 611-valid-triangle-number/valid-triangle-number.py
+++ 611-valid-triangle-number/valid-triangle-number.py
@@ -2,15 +2,15 @@ class Solution:
     def triangleNumber(self, nums: List[int]) -> int:
         nums.sort()
         total = 0
-        for a in range(len(nums)):
-            for b in range(a+1, len(nums)):
-                l, r = b+1, len(nums)-1
-                while l < r:
-                    m = (l + r + 1) // 2
-                    if nums[m] >= nums[a] + nums[b]:
-                        r = m - 1
-                    else:
-                        l = m
-                if b < l < len(nums) and nums[l] < nums[a] + nums[b]:
-                    total += (l - b)
+        c = len(nums)-1
+        while c >= 0:
+            a = 0
+            b = c-1
+            while a < b:
+                if nums[a] + nums[b] > nums[c]:
+                    total += b - a
+                    b -= 1
+                else:
+                    a += 1
+            c -= 1
         return total
