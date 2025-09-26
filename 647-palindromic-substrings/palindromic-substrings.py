a787c9062b6e1611699dcbc38710ef339b746a3a
andrewaucie <aucieandrew@gmail.com>
2025-09-26 17:19:10 -0400

    Time: 131 ms (56.24%) | Memory: 17.9 MB (51.02%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.py
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -1,48 +1,19 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        # Brute force
-        # def isPalindrome(left, right):
-        #     while left <= right and s[left] == s[right]:
-        #         left += 1
-        #         right -= 1
-        #     return left > right
-        
-        # res = 0
-        # for i in range(len(s)):
-        #     for j in range(i, len(s)):
-        #         if isPalindrome(i,j):
-        #             res += 1
-        # return res
-
-        # Expand around centers
-        def expand(left, right):
-            c = 0
+        total = 0
+        for center in range(len(s)):
+            # odd length palindrome: l [c] r
+            left, right = center, center
             while left >= 0 and right < len(s) and s[left] == s[right]:
                 left -= 1
                 right += 1
-                c += 1
-            return c
-
-        res = 0
-        for i in range(len(s)):
-            odd = expand(i,i)
-            even = expand(i, i+1)
-            res += even + odd
-        return res
-
-        if len(s) == 1:
-            return 1
-        dp = [[False] * len(s) for _ in range(len(s))]
-        for i in range(len(s)):
-            dp[i][i] = True
-            if i < len(s)-1:
-                dp[i][i+1] = True
-        count = len(s)
-        for i in range(len(s)):
-            for j in range(i+1, len(s)):
-                if dp[i+1][j-1] and s[i] == s[j]:
-                    dp[i][j] = True
-                    count += 1
-        return count
-        
-            
+                total += 1
+            # even length palindrome: l [c] [c] r
+            if center < len(s)-1 and s[center] == s[center+1]:
+                left = center
+                right = center + 1
+                while left >= 0 and right < len(s) and s[left] == s[right]:
+                    left -= 1
+                    right += 1
+                    total += 1
+        return total
