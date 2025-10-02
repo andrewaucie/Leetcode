bc23460d338282eb44f35d5b961cf3c3915eca1a
andrewaucie <aucieandrew@gmail.com>
2025-10-02 02:16:36 -0400

    Time: 4 ms (54.91%) | Memory: 19.8 MB (76.89%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/meeting-rooms-ii.py
@@ -1,17 +1,24 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        intervals.sort(key=lambda x:(x[0],x[1]))
-        overlapping = list(intervals)
-        res = 0
-        while overlapping:
-            nextOverlapping = []
-            prevEnd = overlapping[0][1]
-            for i in range(1, len(overlapping)):
-                if overlapping[i][0] < prevEnd:
-                    nextOverlapping.append(overlapping[i])
-                else:
-                    prevEnd = overlapping[i][1]
-            overlapping = nextOverlapping
-            res += 1
-        return res    
-            
+        # sort intervals
+        # heap to track current ongoing meetings (based on end time)
+        # if start >= heap[0] (soonest meeting to finish):
+        #      pop heap
+        # add current meeting
+        # [4] -> check if 5 >= 4 (True)
+        # pop heap -> [] -> add 5 as this is now ongoing -> [5]
+
+        # [0,30], [5,10], [30,40], [40,50], [41, 51]
+        # [30] -> [10, 30] -> [30, 40] -> [40, 50] -> [41, 50]
+        # ---------
+        #  -- --
+        #   --
+        intervals.sort()
+        heap = []
+        maxMeetings = 0
+        for start, end in intervals:
+            while heap and start >= heap[0]:
+                heapq.heappop(heap)
+            heapq.heappush(heap, end)
+            maxMeetings = max(maxMeetings, len(heap))
+        return maxMeetings
